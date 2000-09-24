61e8a40f0d4e886bec96c3f9ee90433a3fab9d54
Alan Hourihane <alanh@fairlite.demon.co.uk>
2000-09-24 13:51:22 +0000

    DRI merge

--- hw/kdrive/trident/trident.c
+++ hw/kdrive/trident/trident.c
@@ -21,7 +21,7 @@
  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
  */
-/* $XFree86: xc/programs/Xserver/hw/kdrive/trident/trident.c,v 1.8 2000/09/03 05:11:20 keithp Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/kdrive/trident/trident.c,v 1.9 2000/09/15 07:25:12 keithp Exp $ */
 
 #include "trident.h"
 #define extern
