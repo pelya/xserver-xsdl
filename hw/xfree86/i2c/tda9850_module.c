ebe7b3fe160259b6f19fe760d6ff4f5bb1dd4b72
Vladimir Dergachev <volodya@mindspring.com>
2004-10-02 01:35:33 +0000

    xc/programs/Xserver/hw/xfree86/drivers/i2c/*_module.c Change version     strings to XORG.

--- hw/xfree86/i2c/tda9850_module.c
+++ hw/xfree86/i2c/tda9850_module.c
@@ -10,7 +10,7 @@ static XF86ModuleVersionInfo tda9850VersRec =
         MODULEVENDORSTRING,
         MODINFOSTRING1,
         MODINFOSTRING2,
-        XF86_VERSION_CURRENT,
+        XORG_VERSION_CURRENT,
         1, 0, 0,
         ABI_CLASS_VIDEODRV,             /* This needs the video driver ABI */
         ABI_VIDEODRV_VERSION,
