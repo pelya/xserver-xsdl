fa5539247d3b246db9ff1469d08167178c85d7ad
Daniel Stone <daniel@fooishbar.org>
2005-04-20 12:39:28 +0000

    Change Xalloca.h to X11/Xalloca.h for include.

--- include/os.h
+++ include/os.h
@@ -54,7 +54,7 @@ SOFTWARE.
 #include "misc.h"
 #define ALLOCATE_LOCAL_FALLBACK(_size) Xalloc((unsigned long)(_size))
 #define DEALLOCATE_LOCAL_FALLBACK(_ptr) Xfree((pointer)(_ptr))
-#include "Xalloca.h"
+#include <X11/Xalloca.h>
 #ifndef IN_MODULE
 #include <stdarg.h>
 #else
