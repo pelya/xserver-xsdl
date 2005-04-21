b241c703a2c4164420dd26ee11f583bbf9cfe0f3
Daniel Stone <daniel@fooishbar.org>
2005-04-21 00:31:13 +0000

    Change keymap.h includes to X11/keymap.h

--- hw/xfree86/common/atKeynames.h
+++ hw/xfree86/common/atKeynames.h
@@ -55,8 +55,8 @@
 
 #define XK_TECHNICAL
 #define	XK_KATAKANA
-#include "keysym.h"
-#include "XF86keysym.h"
+#include <X11/keysym.h>
+#include <X11/XF86keysym.h>
 
 #define GLYPHS_PER_KEY	4
 #define NUM_KEYCODES	248
