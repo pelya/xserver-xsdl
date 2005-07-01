9a6ec34d2545a23586f11ad51c81f41a940d73d0
Daniel Stone <daniel@fooishbar.org>
2005-07-01 21:13:36 +0000

    Use canonical autogen.sh, which supports srcdir != objdir autogen.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
 autoreconf -v --install || exit 1
-./configure --enable-maintainer-mode "$@"
+cd $ORIGDIR || exit $?
+
+$srcdir/configure --enable-maintainer-mode "$@"
