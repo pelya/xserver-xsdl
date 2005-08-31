a65c5796133d126b1810749b5206607e7c42d787
Kristian Høgsberg <krh@redhat.com>
2005-08-31 14:58:09 +0000

    Include xkb-config.h if we have it so we pick up the paths defined there.

--- xkb/ddxLoad.c
+++ xkb/ddxLoad.c
@@ -30,6 +30,10 @@ THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <dix-config.h>
 #endif
 
+#ifdef HAVE_XKB_CONFIG_H
+#include <xkb-config.h>
+#endif
+
 #include <stdio.h>
 #include <ctype.h>
 #define	NEED_EVENTS 1
--- xkb/xkbInit.c
+++ xkb/xkbInit.c
@@ -31,6 +31,10 @@ THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <dix-config.h>
 #endif
 
+#ifdef HAVE_XKB_CONFIG_H
+#include <xkb-config.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
