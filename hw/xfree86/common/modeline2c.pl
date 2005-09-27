1a4e30d508e62ab304722c3525748ff0e3c2899d
Kevin E Martin <kem@kem.org>
2005-09-27 13:47:26 +0000

    Include xorg-config.h so the generated file will also include it.

--- hw/xfree86/common/modeline2c.pl
+++ hw/xfree86/common/modeline2c.pl
@@ -67,6 +67,10 @@ printf("/* \$$proj\$ */
  * Author: Dirk Hohndel <hohndel\@XFree86.Org>
  */
 
+#ifdef HAVE_XORG_CONFIG_H
+#include <xorg-config.h>
+#endif
+
 #ifdef __UNIXOS2__
 #define I_NEED_OS2_H
 #endif
