b61c828b0455ec1d4a7ffb54b5ac9b65764a458b
Kevin E Martin <kem@kem.org>
2005-10-18 02:23:58 +0000

    DRM 20051017 import

--- hw/xfree86/os-support/xf86drm.h
+++ hw/xfree86/os-support/xf86drm.h
@@ -287,7 +287,7 @@ typedef struct _drmSetVersion {
 #define DRM_LOCK_CONT  0x40000000U /**< Hardware lock is contended */
 
 #if defined(__GNUC__) && (__GNUC__ >= 2)
-# if defined(__i386) || defined(__AMD64__)
+# if defined(__i386) || defined(__AMD64__) || defined(__x86_64__) || defined(__amd64__)
 				/* Reflect changes here to drmP.h */
 #define DRM_CAS(lock,old,new,__ret)                                    \
 	do {                                                           \
