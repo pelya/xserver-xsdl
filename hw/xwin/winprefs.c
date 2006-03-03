b2f8f410c0bb8bc24039b2a593f8a2a483659914
Alan Hourihane <alanh@fairlite.demon.co.uk>
2006-03-03 09:54:54 +0000

    https://bugs.freedesktop.org/show_bug.cgi?id=4341 Make Xming error messages     more meaningful.

--- hw/xwin/winprefs.c
+++ hw/xwin/winprefs.c
@@ -380,7 +380,31 @@ HandleCustomWM_COMMAND (unsigned long hwndIn,
 		  else
 		    return TRUE;
 		  break;
-#endif		  
+#else
+		case CMD_EXEC:
+                  {
+		    /* Start process without console window */
+		    STARTUPINFO start;
+		    PROCESS_INFORMATION child;
+
+		    memset (&start, 0, sizeof (start));
+		    start.cb = sizeof (start);
+		    start.dwFlags = STARTF_USESHOWWINDOW;
+		    start.wShowWindow = SW_HIDE;
+
+		    memset (&child, 0, sizeof (child));
+
+		    if (CreateProcess (NULL, m->menuItem[j].param, NULL, NULL, FALSE, 0,
+				       NULL, NULL, &start, &child))
+		    {
+			CloseHandle (child.hThread);
+			CloseHandle (child.hProcess);
+		    }
+		    else
+			MessageBox(NULL, m->menuItem[j].param, "Mingrc Exec Command Error!", MB_OK | MB_ICONEXCLAMATION);
+                  }
+		  return TRUE;
+#endif
 		case CMD_ALWAYSONTOP:
 		  if (!hwnd)
 		    return FALSE;
