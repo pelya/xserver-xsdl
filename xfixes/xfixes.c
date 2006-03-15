152090ce442e94de1ae920208a92931af6493c8c
Adam Jackson <ajax@nwnk.net>
2006-03-15 16:33:12 +0000

    Coverity #487: Check version number correctly.

--- xfixes/xfixes.c
+++ xfixes/xfixes.c
@@ -143,7 +143,7 @@ ProcXFixesDispatch (ClientPtr client)
     REQUEST(xXFixesReq);
     XFixesClientPtr pXFixesClient = GetXFixesClient (client);
 
-    if (pXFixesClient->major_version > NUM_VERSION_REQUESTS)
+    if (pXFixesClient->major_version >= NUM_VERSION_REQUESTS)
 	return BadRequest;
     if (stuff->xfixesReqType > version_requests[pXFixesClient->major_version])
 	return BadRequest;
