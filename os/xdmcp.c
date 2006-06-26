b3c869304cd85af034aa9debaa874e29d14fcbe6
Peter Breitenlohner <peb@mppmu.mpg.de>
2006-06-26 10:48:44 -0700

    Free small, one-time memory leak in xdmcp -from handling
    Part of Patch #6046 <https://bugs.freedesktop.org/attachment.cgi?id=6046>

--- os/xdmcp.c
+++ os/xdmcp.c
@@ -1603,6 +1603,10 @@ get_fromaddr_by_name(
       , &ai, &aifirst
 #endif
 	);
+#if defined(IPv6) && defined(AF_INET6)
+    if (aifirst != NULL)
+	freeaddrinfo(aifirst);
+#endif
     xdm_from = argv[i];
 }
 
