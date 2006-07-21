aff404f293ed86a44a093a51a9f11e79e6c3f4f6
Adam Jackson <ajax@theobromine.nwnk.net>
2006-07-21 18:24:37 -0400

    Detach xf4bpp from cfb.

--- hw/xfree86/xf4bpp/ppcCpArea.c
+++ hw/xfree86/xf4bpp/ppcCpArea.c
@@ -55,8 +55,7 @@ SOFTWARE.
 #include "xf4bpp.h"
 #include "mfbmap.h"
 #define PSZ 8
-#include "cfb.h"
-#include "cfbmskbits.h"
+#include "mfb.h"
 #include "mergerop.h"
 #include "mi.h"
 #include "pixmapstr.h"
@@ -87,8 +86,8 @@ vga16DoBitblt
     int w, h;
     int careful;
 
-    widthSrc = cfbGetLongWidth(pSrc);
-    widthDst = cfbGetLongWidth(pDst);
+    widthSrc = mfbGetPixelWidth(pSrc);
+    widthDst = mfbGetPixelWidth(pDst);
 
     /* XXX we have to err on the side of safety when both are windows,
      * because we don't know if IncludeInferiors is being used.
