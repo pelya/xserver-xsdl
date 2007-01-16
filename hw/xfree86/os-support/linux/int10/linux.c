f36a447d20c0a89b78c5899eeb303cab568ebac6
Ian Romanick <idr@us.ibm.com>
2007-01-16 14:49:19 -0800

    Eliminate use of Tag field.
    The Tag field was removed from the int10 structures in commit
    ca9c41e09ddb72d150d86f5d372eb07eeca7c1a2.  This file was over looked.

--- hw/xfree86/os-support/linux/int10/linux.c
+++ hw/xfree86/os-support/linux/int10/linux.c
@@ -90,7 +90,6 @@ xf86ExtendedInitInt10(int entityIndex, int Flags)
     legacyVGARec vga;
     xf86int10BiosLocation bios;
     Bool videoBiosMapped = FALSE;
-    pciVideoPtr pvp;
     
     if (int10Generation != serverGeneration) {
 	counter = 0;
@@ -152,8 +151,8 @@ xf86ExtendedInitInt10(int entityIndex, int Flags)
     pInt = (xf86Int10InfoPtr)xnfcalloc(1, sizeof(xf86Int10InfoRec));
     pInt->scrnIndex = screen;
     pInt->entityIndex = entityIndex;
-    pvp = xf86GetPciInfoForEntity(entityIndex);
-    if (pvp) pInt->Tag = pciTag(pvp->bus, pvp->device, pvp->func);
+    pInt->dev = xf86GetPciInfoForEntity(entityIndex);
+
     if (!xf86Int10ExecSetup(pInt))
 	goto error0;
     pInt->mem = &linuxMem;
