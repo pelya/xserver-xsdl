a3d2c5d622d9ca36d6fa2966aff09524e3ea39ac
Adam Jackson <ajax@benzedrine.nwnk.net>
2007-03-07 11:02:47 -0500

    XORG_VERSION_CURRENT, not XF86_VERSION_CURRENT.
    If only this was the least wrong thing in this code.

--- hw/xfree86/i2c/uda1380_module.c
+++ hw/xfree86/i2c/uda1380_module.c
@@ -12,7 +12,7 @@ static XF86ModuleVersionInfo uda1380VersRec =
         MODULEVENDORSTRING,
         MODINFOSTRING1,
         MODINFOSTRING2,
-        XF86_VERSION_CURRENT,
+        XORG_VERSION_CURRENT,
         1, 0, 0,
         ABI_CLASS_VIDEODRV,             /* This needs the video driver ABI */
         ABI_VIDEODRV_VERSION,
