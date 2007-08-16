1d4bea6106d7a1c83e1dfe37fad8268589feaa0b
Jeremy C. Reed <reed@glacier.reedmedia.net>
2007-08-16 11:20:12 -0500

    Add some more support for DragonFly. From Joerg Sonnenberger and pkgsrc.

--- hw/xfree86/os-support/bus/Pci.h
+++ hw/xfree86/os-support/bus/Pci.h
@@ -235,7 +235,7 @@
 # if defined(linux)
 #  define ARCH_PCI_INIT axpPciInit
 #  define INCLUDE_XF86_MAP_PCI_MEM
-# elif defined(__FreeBSD__) || defined(__OpenBSD__)
+# elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
 #  define ARCH_PCI_INIT freebsdPciInit
 #  define INCLUDE_XF86_MAP_PCI_MEM
 #  define INCLUDE_XF86_NO_DOMAIN
