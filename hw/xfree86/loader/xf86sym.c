1f6ddae003ec65d6bc567831bf32bf75dfefdd6c
Alex Deucher <alex@botch2.com>
2007-08-21 00:37:33 -0400

    add xf86_crtc_clip_video_helper to xf86sym.c

--- hw/xfree86/loader/xf86sym.c
+++ hw/xfree86/loader/xf86sym.c
@@ -1203,6 +1203,7 @@ _X_HIDDEN void *xfree86LookupTab[] = {
     SYMFUNC(xf86_show_cursors)
     SYMFUNC(xf86_hide_cursors)
     SYMFUNC(xf86_cursors_fini)
+    SYMFUNC(xf86_crtc_clip_video_helper)
 
     SYMFUNC(xf86DoEDID_DDC1)
     SYMFUNC(xf86DoEDID_DDC2)
