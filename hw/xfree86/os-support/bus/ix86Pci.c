1d938a80fd4fa58d1791c146b6b5c2dfe148dce7
Alan Coopersmith <alan.coopersmith@sun.com>
2007-09-26 19:42:46 -0700

    Remove unused pciAddrHostToBus functions from ix86Pci.c
    Matches linuxPci.c changes made in 8279444a54f38c5e2e5d4c2d936a10d74990e0be
    Fixes compiler errors:
     "ix86Pci.c", line 194: too many struct/union initializers
     "ix86Pci.c", line 204: too many struct/union initializers
     "ix86Pci.c", line 214: too many struct/union initializers

--- hw/xfree86/os-support/bus/ix86Pci.c
+++ hw/xfree86/os-support/bus/ix86Pci.c
@@ -188,8 +188,8 @@ static pciBusFuncs_t ix86Funcs0 = {
 /* pciReadLong      */	ix86PciReadLongSetup,
 /* pciWriteLong     */	ix86PciWriteLongSetup,
 /* pciSetBitsLong   */	ix86PciSetBitsLongSetup,
-#endif
 /* pciAddrHostToBus */	pciAddrNOOP,
+#endif
 /* pciAddrBusToHost */	pciAddrNOOP
 };
 
@@ -198,8 +198,8 @@ static pciBusFuncs_t ix86Funcs1 = {
 /* pciReadLong      */	ix86PciReadLongCFG1,
 /* pciWriteLong     */	ix86PciWriteLongCFG1,
 /* pciSetBitsLong   */	ix86PciSetBitsLongCFG1,
-#endif
 /* pciAddrHostToBus */	pciAddrNOOP,
+#endif
 /* pciAddrBusToHost */	pciAddrNOOP
 };
 
@@ -208,8 +208,8 @@ static pciBusFuncs_t ix86Funcs2 = {
 /* pciReadLong      */	ix86PciReadLongCFG2,
 /* pciWriteLong     */	ix86PciWriteLongCFG2,
 /* pciSetBitsLong   */	ix86PciSetBitsLongCFG2,
-#endif
 /* pciAddrHostToBus */	pciAddrNOOP,
+#endif
 /* pciAddrBusToHost */	pciAddrNOOP
 };
 
