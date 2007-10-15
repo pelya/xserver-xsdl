2c9646ad4e65bb061d910c9e2b1a8a978f21fa17
Eamon Walsh <ewalsh@tycho.nsa.gov>
2007-10-15 19:18:03 -0400

    registry: Register SHM extension protocol names.

--- Xext/shm.c
+++ Xext/shm.c
@@ -59,6 +59,7 @@ in this Software without prior written authorization from The Open Group.
 #include "servermd.h"
 #include "shmint.h"
 #include "xace.h"
+#include "registry.h"
 #define _XSHM_SERVER_
 #include <X11/extensions/shmstr.h>
 #include <X11/Xfuncproto.h>
@@ -246,7 +247,27 @@ ShmExtensionInit(INITARGS)
 	ShmCompletionCode = extEntry->eventBase;
 	BadShmSegCode = extEntry->errorBase;
 	EventSwapVector[ShmCompletionCode] = (EventSwapPtr) SShmCompletionEvent;
-    }
+    } else
+	return;
+
+    RegisterRequestName(ShmReqCode, X_ShmQueryVersion,
+			SHMNAME ":QueryVersion");
+    RegisterRequestName(ShmReqCode, X_ShmAttach,
+			SHMNAME ":Attach");
+    RegisterRequestName(ShmReqCode, X_ShmDetach,
+			SHMNAME ":Detach");
+    RegisterRequestName(ShmReqCode, X_ShmPutImage,
+			SHMNAME ":PutImage");
+    RegisterRequestName(ShmReqCode, X_ShmGetImage,
+			SHMNAME ":GetImage");
+    RegisterRequestName(ShmReqCode, X_ShmCreatePixmap,
+			SHMNAME ":CreatePixmap");
+
+    RegisterEventName(extEntry->eventBase + ShmCompletion,
+		      SHMNAME ":Completion");
+
+    RegisterErrorName(extEntry->errorBase + BadShmSeg,
+		      SHMNAME ":BadShmSeg");
 }
 
 /*ARGSUSED*/
