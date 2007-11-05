fb32bb9839b615f7297fbfac2050bc216682f01c
Daniel Stone <daniel@fooishbar.org>
2007-11-05 14:17:54 +0000

    Xi: Remove usage of alloca
    Replace with xalloc/xfree.

--- Xi/gtmotion.c
+++ Xi/gtmotion.c
@@ -135,7 +135,7 @@ ProcXGetDeviceMotionEvents(ClientPtr client)
     if (num_events) {
 	size = sizeof(Time) + (axes * sizeof(INT32));
 	tsize = num_events * size;
-	coords = (INT32 *) ALLOCATE_LOCAL(tsize);
+	coords = (INT32 *) xalloc(tsize);
 	if (!coords)
 	    return BadAlloc;
 	rep.nEvents = (v->GetMotionProc) (dev, (xTimecoord *) coords,	/* XXX */
@@ -161,7 +161,7 @@ ProcXGetDeviceMotionEvents(ClientPtr client)
 	WriteToClient(client, length * 4, (char *)coords);
     }
     if (coords)
-	DEALLOCATE_LOCAL(coords);
+	xfree(coords);
     return Success;
 }
 
