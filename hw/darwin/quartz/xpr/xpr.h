b4d14484056e6f4a7374fc1acf3f223be4bd116f
Ben Byer <bbyer@bbyer.local>
2007-11-07 03:10:52 -0800

    Undo some last-minute breakage in xpr.h

--- hw/darwin/quartz/xpr/xpr.h
+++ hw/darwin/quartz/xpr/xpr.h
@@ -38,8 +38,6 @@ void AppleDRIExtensionInit(void);
 void xprAppleWMInit(void);
 Bool xprInit(ScreenPtr pScreen);
 Bool xprIsX11Window(void *nsWindow, int windowNumber);
-WindowPtr xprGetX11Window(xp_window_id wid);
-WindowPtr xprGetXWindowFromAppKit(int windowNumber);
 
 void xprHideWindows(Bool hide);
 
