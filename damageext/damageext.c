b9ab6f300a46aa8879b11eac51857357cc379c2f
Eamon Walsh <ewalsh@tycho.nsa.gov>
2007-11-20 17:31:00 -0500

    Revert "registry: Register DAMAGE extension protocol names."
    This reverts commit 20db50b4c44a14f7eeac2b1de17ada68482521da.
    
    Moving all the names into dix/registry.c

--- damageext/damageext.c
+++ damageext/damageext.c
@@ -25,7 +25,6 @@
 #endif
 
 #include "damageextint.h"
-#include "registry.h"
 
 static unsigned char	DamageReqCode;
 static int		DamageEventBase;
@@ -527,23 +526,5 @@ DamageExtensionInit(void)
 	DamageErrorBase = extEntry->errorBase;
 	EventSwapVector[DamageEventBase + XDamageNotify] =
 			(EventSwapPtr) SDamageNotifyEvent;
-    } else
-	return;
-
-    RegisterRequestName(DamageReqCode, X_DamageQueryVersion,
-			DAMAGE_NAME ":QueryVersion");
-    RegisterRequestName(DamageReqCode, X_DamageCreate,
-			DAMAGE_NAME ":Create");
-    RegisterRequestName(DamageReqCode, X_DamageDestroy,
-			DAMAGE_NAME ":Destroy");
-    RegisterRequestName(DamageReqCode, X_DamageSubtract,
-			DAMAGE_NAME ":Subtract");
-    RegisterRequestName(DamageReqCode, X_DamageAdd,
-			DAMAGE_NAME ":Add");
-
-    RegisterEventName(DamageEventBase + XDamageNotify,
-			DAMAGE_NAME ":Notify");
-
-    RegisterErrorName(extEntry->errorBase + BadDamage,
-			DAMAGE_NAME ":BadDamage");
+    }
 }
