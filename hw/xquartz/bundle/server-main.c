02df03667052fa6a4e0405b91a005dc48e9b39c4
Jeremy Huddleston <jeremy@yuffie.local>
2007-12-08 01:28:26 -0800

    Xquartz: Actually, it should be org.x.X11 for case-sensitive FS (cherry picked from commit c5ccb98d5d461c8a22fc0f3942a607ac90e1e37e)

--- hw/xquartz/bundle/server-main.c
+++ hw/xquartz/bundle/server-main.c
@@ -138,7 +138,7 @@ read_boolean_pref (CFStringRef name, int default_)
     int value;
     Boolean ok;
 	
-    value = CFPreferencesGetAppBooleanValue (name, CFSTR ("org.x.x11"), &ok);
+    value = CFPreferencesGetAppBooleanValue (name, CFSTR ("org.x.X11"), &ok);
     return ok ? value : default_;
 }
 
