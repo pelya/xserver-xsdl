7cdc19b29d93bf15cecfd6b69e269fab2501bca0
Alan Coopersmith <alan.coopersmith@sun.com>
2008-05-13 16:39:30 -0700

    When XKB fails to open rules file, log the file name, not the NULL file pointer

--- xkb/ddxLoad.c
+++ xkb/ddxLoad.c
@@ -401,7 +401,7 @@ XkbRF_RulesPtr	rules;
 
     file = fopen(buf, "r");
     if (!file) {
-        LogMessage(X_ERROR, "XKB: Couldn't open rules file %s\n", file);
+        LogMessage(X_ERROR, "XKB: Couldn't open rules file %s\n", buf);
 	return False;
     }
 
