74372fd0049676787904f1f9f80f836b97f5f60e
Peter Hutterer <peter@cs.unisa.edu.au>
2008-05-22 18:02:10 +0930

    xfree86: suspend signals while removing a device.
    Getting a keyboard event while halfway through freeing memory can be
    unpleasant.

--- hw/xfree86/common/xf86Xinput.c
+++ hw/xfree86/common/xf86Xinput.c
@@ -475,6 +475,7 @@ DeleteInputDeviceRequest(DeviceIntPtr pDev)
         drv = pInfo->drv;
         idev = pInfo->conf_idev;
     }
+    OsBlockSignals();
     RemoveDevice(pDev);
 
     if (pDev->isMaster)
@@ -484,6 +485,7 @@ DeleteInputDeviceRequest(DeviceIntPtr pDev)
         drv->UnInit(drv, pInfo, 0);
     else
         xf86DeleteInput(pInfo, 0);
+    OsReleaseSignals();
 
     /* devices added through HAL aren't in the config layout */
     it = xf86ConfigLayout.inputs;
