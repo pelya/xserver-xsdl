ff3adf3e564d94fea18e48f966de40a7ded1279e
Peter Hutterer <peter@cs.unisa.edu.au>
2008-06-04 11:10:11 +0930

    xkb: reset xkb_cached_map on CloseDownDevices.
    Could lead to some invalid pointers in the second server generation.

--- xkb/xkbInit.c
+++ xkb/xkbInit.c
@@ -292,6 +292,9 @@ XkbDeleteRulesDflts()
     XkbVariantDflt = NULL;
     _XkbFree(XkbOptionsDflt);
     XkbOptionsDflt = NULL;
+
+    XkbFreeKeyboard(xkb_cached_map, XkbAllComponentsMask, True);
+    xkb_cached_map = NULL;
 }
 
 /***====================================================================***/
