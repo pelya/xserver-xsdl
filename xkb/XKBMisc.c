5544c51447f551dfc6df64438873a7ce64743976
Eamon Walsh <ewalsh@tycho.nsa.gov>
2008-06-13 22:49:47 -0400

    Fix "warning: the address of ‘newTypes’ will always evaluate as ‘true’".

--- xkb/XKBMisc.c
+++ xkb/XKBMisc.c
@@ -473,7 +473,7 @@ register int	i;
 int		width,nOldGroups,oldWidth,newTypes[XkbNumKbdGroups];
 
     if ((!xkb) || (!XkbKeycodeInRange(xkb,key)) || (!xkb->map) ||
-	(!xkb->map->types)||(!newTypes)||((groups&XkbAllGroupsMask)==0)||
+	(!xkb->map->types)||((groups&XkbAllGroupsMask)==0)||
 	(nGroups>XkbNumKbdGroups)) {
 	return BadMatch;
     }
