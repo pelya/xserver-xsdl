7ddfb2f1587da3ec1cd27ae270e42700adf4f560
Eamon Walsh <ewalsh@tycho.nsa.gov>
2008-06-26 21:23:31 -0400

    Fix a leak of window devPrivates in window.c/CrushTree().

--- dix/window.c
+++ dix/window.c
@@ -929,6 +929,7 @@ CrushTree(WindowPtr pWin)
 		(*UnrealizeWindow)(pChild);
 	    }
 	    FreeWindowResources(pChild);
+	    dixFreePrivates(pChild->devPrivates);
 	    xfree(pChild);
 	    if ( (pChild = pSib) )
 		break;
