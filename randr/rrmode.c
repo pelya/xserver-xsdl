7c5ca85a9e6d49ab572831b3e0c08bb4cafb395e
Zhenyu Wang <zhenyu.z.wang@intel.com>
2008-09-03 09:41:00 +0800

    randr: fix user mode create initial reference count
    Don't need extra reference count adding when creating user mode.
    This fixes user mode destroy, otherwise we get BadAccess error.

--- randr/rrmode.c
+++ randr/rrmode.c
@@ -85,11 +85,6 @@ RRModeCreate (xRRModeInfo   *modeInfo,
 	return NULL;
     modes = newModes;
     modes[num_modes++] = mode;
-    
-    /*
-     * give the caller a reference to this mode
-     */
-    ++mode->refcnt;
     return mode;
 }
 
