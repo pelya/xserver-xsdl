63abdf61151cc90f769ed1708ee19b6c434c27c9
George Staplin <georgestaplin@george-staplins-mac-pro.local>
2008-10-06 18:05:12 -0600

    XQuartz: GL: Add the new C code to the Makefile.am.
    Commit the darwin.c changes I missed in the last commit, for calling
    setVisualConfigs().
    (cherry picked from commit eb3c014e1710bf0b93bda10ddb9b795cd150d02d)

--- hw/xquartz/darwin.c
+++ hw/xquartz/darwin.c
@@ -75,6 +75,9 @@
 #include "quartz.h"
 //#include "darwinClut8.h"
 
+#include "GL/visualConfigs.h"
+
+
 #ifdef ENABLE_DEBUG_LOG
 FILE *debug_log_fp = NULL;
 #endif
@@ -179,7 +182,6 @@ static Bool DarwinSaveScreen(ScreenPtr pScreen, int on)
     return TRUE;
 }
 
-
 /*
  * DarwinAddScreen
  *  This is a callback from dix during AddScreen() from InitOutput().
@@ -225,6 +227,8 @@ static Bool DarwinAddScreen(int index, ScreenPtr pScreen, int argc, char **argv)
         miSetVisualTypesAndMasks(24, LARGE_VISUALS, 8, TrueColor, 0x00ff0000, 0x0000ff00, 0x000000ff);
 
     miSetPixmapDepths();
+
+    setVisualConfigs();
 
     // machine independent screen init
     // setup _Screen structure in pScreen
