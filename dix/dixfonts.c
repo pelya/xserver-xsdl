e85d5d707d51d6a1558dfcf0ebd50183b84be39d
Julien Cristau <jcristau@debian.org>
2008-10-26 11:55:50 +0100

    Ansify declaration of FontToXError()

--- dix/dixfonts.c
+++ dix/dixfonts.c
@@ -98,8 +98,7 @@ static FontPathElementPtr *slept_fpes = (FontPathElementPtr *) 0;
 static FontPatternCachePtr patternCache;
 
 static int
-FontToXError(err)
-    int         err;
+FontToXError(int err)
 {
     switch (err) {
     case Successful:
