3e6da1636093d7dc98baac40544c0b0fb7fd8aec
Peter Hutterer <peter.hutterer@redhat.com>
2008-11-14 15:55:57 +1000

    include: remove ENTER_LEAVE_SEMAPHORE macros.

--- include/input.h
+++ include/input.h
@@ -97,12 +97,6 @@ SOFTWARE.
 #define SEMAPHORE_FIELD_UNSET(win, dev, field) \
     (win)->field[(dev)->id/8] &= ~(1 << ((dev)->id % 8));
 
-#define ENTER_LEAVE_SEMAPHORE_SET(win, dev) \
-        SEMAPHORE_FIELD_SET(win, dev, enterleave);
-
-#define ENTER_LEAVE_SEMAPHORE_UNSET(win, dev) \
-        SEMAPHORE_FIELD_UNSET(win, dev, enterleave);
-
 #define FOCUS_SEMAPHORE_SET(win, dev) \
         SEMAPHORE_FIELD_SET(win, dev, focusinout);
 
