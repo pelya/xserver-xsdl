09bfb25e031772611a2f0902d4ba77b587e4bdb2
Adam Jackson <ajax@redhat.com>
2008-11-25 00:43:36 -0500

    Remove unused XAAAvailableOptions

--- hw/xfree86/xaa/xaaInitAccel.c
+++ hw/xfree86/xaa/xaaInitAccel.c
@@ -16,8 +16,6 @@
 #include "xf86fbman.h"
 #include "servermd.h"
 
-static const OptionInfoRec *XAAAvailableOptions(void *unused);
-
 /*
  * XAA Config options
  */
@@ -110,13 +108,6 @@ static XF86ModuleVersionInfo xaaVersRec =
 };
 
 _X_EXPORT XF86ModuleData xaaModuleData = { &xaaVersRec, NULL, NULL };
-
-/*ARGSUSED*/
-static const OptionInfoRec *
-XAAAvailableOptions(void *unused)
-{
-    return (XAAOptions);
-}
 
 Bool
 XAAInitAccel(ScreenPtr pScreen, XAAInfoRecPtr infoRec)
