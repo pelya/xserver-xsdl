ef4179f43a84d90f867b95ee5974ad26884253b5
Jeremy Huddleston <jeremyhu@freedesktop.org>
2008-11-26 12:15:58 -0800

    XQuartz: Fix dead-acute on Greek keyboards (cherry picked from commit 807f2ec35c5f95b56b3e2b5eac51aec300fe5cb6)

--- hw/xquartz/quartzKeyboard.c
+++ hw/xquartz/quartzKeyboard.c
@@ -145,8 +145,9 @@ const static struct {
     KeySym normal, dead;
 } dead_keys[] = {
     {XK_grave, XK_dead_grave},
-    {XK_apostrophe, XK_dead_acute},         /* "=" on a Czech keyboard */
+    {XK_apostrophe, XK_dead_acute},             /* US:"=" on a Czech keyboard */
     {XK_acute, XK_dead_acute},
+    {UKEYSYM (0x384), XK_dead_acute},           /* US:";" on a Greek keyboard */
     {XK_asciicircum, XK_dead_circumflex},
     {UKEYSYM (0x2c6), XK_dead_circumflex},	/* MODIFIER LETTER CIRCUMFLEX ACCENT */
     {XK_asciitilde, XK_dead_tilde},
