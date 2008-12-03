8e3279134987a45f2a89c963ef2d33bc3d3c8179
Peter Hutterer <peter.hutterer@redhat.com>
2008-12-04 09:47:25 +1000

    xfree86: fix compiler warning (use of uninitialized variable)
    drv and idev are only set for SDs, but are only dereferenced for SDs too, so
    initializing them to NULL is safe.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@redhat.com>

--- hw/xfree86/common/xf86Xinput.c
+++ hw/xfree86/common/xf86Xinput.c
@@ -650,8 +650,8 @@ void
 DeleteInputDeviceRequest(DeviceIntPtr pDev)
 {
     LocalDevicePtr pInfo = (LocalDevicePtr) pDev->public.devicePrivate;
-    InputDriverPtr drv;
-    IDevRec *idev;
+    InputDriverPtr drv = NULL;
+    IDevRec *idev = NULL;
     IDevPtr *it;
     Bool isMaster = pDev->isMaster;
 
