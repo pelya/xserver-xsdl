5e0967f5fc2e0fcf2a55128eb03931cf6f5709e9
Peter Hutterer <peter.hutterer@who-t.net>
2008-12-12 10:49:53 +1000

    dix: add SetBit(arr, bit) and ClearBit(arr, bit) to include/inputstr.h
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- include/inputstr.h
+++ include/inputstr.h
@@ -57,6 +57,8 @@ SOFTWARE.
 #include "privates.h"
 
 #define BitIsOn(ptr, bit) (((BYTE *) (ptr))[(bit)>>3] & (1 << ((bit) & 7)))
+#define SetBit(ptr, bit)  (((BYTE *) (ptr))[(bit)>>3] |= (1 << ((bit) & 7)))
+#define ClearBit(ptr, bit) (((BYTE *)(ptr))[(bit)>>3] &= ~(1 << ((bit) & 7)))
 
 #define SameClient(obj,client) \
 	(CLIENT_BITS((obj)->resource) == (client)->clientAsMask)
