997e8c321bb11067591785ba38103a3a5029301d
Jeremy Huddleston <jeremyhu@freedesktop.org>
2008-12-28 01:53:38 -0800

    XQuartz: Try harder to get the user's login environment (cherry picked from commit 6bedaddd78aa04bd303df434a4c49bb87bd7deaa)

--- hw/xquartz/bundle/X11.sh
+++ hw/xquartz/bundle/X11.sh
@@ -1,8 +1,15 @@
-#!/bin/bash --login
+#!/bin/bash
+
+set "$(dirname "$0")"/X11.bin "${@}"
 
 if [ -x ~/.x11run ]; then
-	exec ~/.x11run "$(dirname "$0")"/X11.bin "${@}"
-else
-	exec "$(dirname "$0")"/X11.bin "${@}"
+	exec ~/.x11run "${@}"
 fi
 
+case $(basename "${SHELL}") in
+	bash)          exec -l "${SHELL}" --login -c 'exec "${@}"' - "${@}" ;;
+	ksh|sh|zsh)    exec -l "${SHELL}" -c 'exec "${@}"' - "${@}" ;;
+	csh|tcsh)      exec -l "${SHELL}" -c 'exec $argv:q' "${@}" ;;
+	es|rc)         exec -l "${SHELL}" -l -c 'exec $*' "${@}" ;;
+	*)             exec    "${@}" ;;
+esac
