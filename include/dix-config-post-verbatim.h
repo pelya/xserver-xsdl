4f004231871c830e16c4525cfaab183fad0aee86
Jeremy Huddleston <jeremyhu@freedesktop.org>
2009-01-07 10:55:08 -0800

    XQuartz: Fat binary buildfixes
    Let Xarch.h set X_BYTE_ORDER for us.
    Determine _XSERVER64 at build time.
    (cherry picked from commit f54736c2b0b537c8322ecb2db8e9cee5eea68165)

---
+++ include/dix-config-post-verbatim.h
@@ -0,0 +1,10 @@
+/* Do not include this file directly.  It is included at the end of <dix-config.h> */
+
+/* Correctly set _XSERVER64 for OSX fat binaries */
+#ifdef __APPLE__
+#if defined(__LP64__) && !defined(_XSERVER64)
+#define _XSERVER64 1
+#elif !defined(__LP64__) && defined(_XSERVER64)
+#undef _XSERVER64
+#endif
+#endif
