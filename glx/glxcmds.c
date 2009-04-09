7b3982eb6518da33ab01c2fbf7ceb45b89f841df
Michel Dänzer <daenzer@vmware.com>
2009-04-09 08:21:09 +0200

    glx: Test the error value, not its address...

--- glx/glxcmds.c
+++ glx/glxcmds.c
@@ -144,7 +144,7 @@ validGlxContext(ClientPtr client, XID id, int access_mode,
 {
     *err = dixLookupResourceByType((pointer *) context, id,
 				   __glXContextRes, client, access_mode);
-    if (err != Success) {
+    if (*err != Success) {
 	client->errorValue = id;
 	if (*err == BadValue)
 	    *err = __glXError(GLXBadContext);
