7fbe1b7d63c94b344e8ff9d09c0bb162ca104909
Peter Hutterer <peter.hutterer@who-t.net>
2009-04-11 13:10:31 +1000

    Xi: remove the GetExtensionVersion hack.
    Squashing data into a request that's defined to be padding is probably not the
    best idea.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- Xi/getvers.c
+++ Xi/getvers.c
@@ -92,7 +92,6 @@ int
 ProcXGetExtensionVersion(ClientPtr client)
 {
     xGetExtensionVersionReply rep;
-    XIClientPtr pXIClient;
 
     REQUEST(xGetExtensionVersionReq);
     REQUEST_AT_LEAST_SIZE(xGetExtensionVersionReq);
@@ -100,21 +99,6 @@ ProcXGetExtensionVersion(ClientPtr client)
     if (stuff->length != (sizeof(xGetExtensionVersionReq) +
 			  stuff->nbytes + 3) >> 2)
 	return BadLength;
-
-    pXIClient = dixLookupPrivate(&client->devPrivates, XIClientPrivateKey);
-
-    /* GetExtensionVersionReq before XI 2 didn't supply the client's
-     * major/minor. So we don't actually have a clue what they support.
-     * {major|minor}Version was added as part of XI, so if they are set, we
-     * know we can trust it. In this case the client must set nbytes to 0
-     * though, otherwise we have to assume that the version are padding
-     * garbage.
-     */
-    if (!stuff->nbytes) /* Client using XQueryInputVersion(). */
-    {
-        pXIClient->major_version = stuff->majorVersion;
-        pXIClient->minor_version = stuff->minorVersion;
-    } /* else version unknown, leave it at 0.0 */
 
     memset(&rep, 0, sizeof(xGetExtensionVersionReply));
     rep.repType = X_Reply;
