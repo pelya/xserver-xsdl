4a36db60774640ea92a2133a3b08fbf1a48d2cb6
Peter Hutterer <peter.hutterer@who-t.net>
2009-05-22 15:16:43 +1000

    os: fix compiler warning "too few arguments to format"
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- os/osinit.c
+++ os/osinit.c
@@ -136,8 +136,8 @@ OsSigHandler(int signo)
 
 #ifdef SA_SIGINFO
   if (sip->si_code == SI_USER) {
-      ErrorF("Recieved signal %d sent by process %ld, uid %ld\n",
-	     (long) sip->si_pid, (long) sip->si_uid);
+      ErrorF("Recieved signal %ld sent by process %ld, uid %ld\n",
+              (long) sip->si_code, (long) sip->si_pid, (long) sip->si_uid);
   } else {
       switch (signo) {
           case SIGSEGV:
