7c7f0c2c6a04f7044d5ce69e97a615735e5831f1
Søren Sandmann Pedersen <sandmann@redhat.com>
2009-07-03 14:36:54 -0400

    Use IncludeInferiors when copying windows before compositing.
    Part of bug 22484.

--- fb/fbpict.c
+++ fb/fbpict.c
@@ -159,9 +159,9 @@ fbComposite (CARD8      op,
 {
     pixman_image_t *src, *mask, *dest;
     
-    miCompositeSourceValidate (pSrc, xSrc - xDst, ySrc - yDst, width, height);
+    miCompositeSourceValidate (pSrc, xSrc, ySrc, width, height);
     if (pMask)
-	miCompositeSourceValidate (pMask, xMask - xDst, yMask - yDst, width, height);
+	miCompositeSourceValidate (pMask, xMask, yMask, width, height);
     
     src = image_from_pict (pSrc, TRUE, TRUE);
     mask = image_from_pict (pMask, TRUE, TRUE);
@@ -295,7 +295,8 @@ copy_drawable (DrawablePtr pDraw)
     
     /* First fill the pixmap with zeros */
     gcv[0].val = 0x00000000;
-    dixChangeGC (NullClient, pGC, GCBackground, NULL, gcv);
+    gcv[1].val = IncludeInferiors;
+    dixChangeGC (NullClient, pGC, GCBackground | GCSubwindowMode, NULL, gcv);
     ValidateGC ((DrawablePtr)pPixmap, pGC);
     miClearDrawable ((DrawablePtr)pPixmap, pGC);
     
