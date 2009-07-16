bb6fa39eb3aa044ffc18632288fd59909ff06344
Adam Jackson <ajax@redhat.com>
2009-07-16 17:43:31 -0400

    dbe: Adapt to new headers

--- dbe/dbestruct.h
+++ dbe/dbestruct.h
@@ -37,10 +37,24 @@
 /* INCLUDES */
 
 #define NEED_DBE_PROTOCOL
-#include <X11/extensions/Xdbeproto.h>
+#include <X11/extensions/dbeproto.h>
 #include "windowstr.h"
 #include "privates.h"
 
+typedef struct
+{
+        VisualID    visual;    /* one visual ID that supports double-buffering */
+            int         depth;     /* depth of visual in bits                      */
+                int         perflevel; /* performance level of visual                  */
+}
+XdbeVisualInfo;
+
+typedef struct
+{
+        int                 count;          /* number of items in visual_depth   */
+            XdbeVisualInfo      *visinfo;       /* list of visuals & depths for scrn */
+}
+XdbeScreenVisualInfo;
 
 /* DEFINES */
 
