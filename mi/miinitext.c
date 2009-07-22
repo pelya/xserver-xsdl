20daa145c437c3ba67970146f6182849f87a1b43
Peter Hutterer <peter.hutterer@who-t.net>
2009-07-23 08:33:00 +1000

    mi: fix build error caused by missing xtest.h
    xtest.h was renamed to xtestconst.h in xextproto.
    Requires xextproto 7.0.99.3
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- mi/miinitext.c
+++ mi/miinitext.c
@@ -159,7 +159,7 @@ typedef void (*InitExtension)(INITARGS);
 #include <X11/extensions/shm.h>
 #endif
 #ifdef XTEST
-#include <X11/extensions/xtest.h>
+#include <X11/extensions/xtestconst.h>
 #endif
 #include <X11/extensions/XKB.h>
 #ifdef XCSECURITY
