654d7a02600bbafd890f555a081aa0a2fcb5e253
Peter Hutterer <peter.hutterer@who-t.net>
2009-08-04 15:23:01 +1000

    Revert "dmx: claim we support XI 2."
    XQueryInputVersion doesn't exist anymore and for now XI 1 support is good
    enough.
    
    This reverts commit 8da8a0fec4b1b9d9208635dedb2f449dc99e0004.

--- hw/dmx/input/dmxinputinit.c
+++ hw/dmx/input/dmxinputinit.c
@@ -845,7 +845,7 @@ static void dmxInputScanForExtensions(DMXInputInfo *dmxInput, int doXI)
     
     /* Print out information about the XInput Extension. */
     handler = XSetExtensionErrorHandler(dmxInputExtensionErrorHandler);
-    ext     = XQueryInputVersion(display, XI_2_Major, XI_2_Minor);
+    ext     = XGetExtensionVersion(display, INAME);
     XSetExtensionErrorHandler(handler);
     
     if (!ext || ext == (XExtensionVersion *)NoSuchExtension) {
