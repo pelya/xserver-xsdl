0f613007cd43545eda40825bd74ada3e6e5f36f6
Jeremy Huddleston <jeremyhu@freedesktop.org>
2009-08-05 17:43:29 -0700

    CheckWindowOptionalNeed: Ensure w->optional is set to avoid SEGFAULT
    If CheckWindowOptionalNeed is called consecutively (and the first removes optional), then
    we will SEGFAULT.  This can happen in XQuartz because ReparentWindow will call
    pScreen->ReparentWindow which can DeleteProperty which will CheckWindowOptionalNeed... then
    ReparentWindow will call it again later indiscriminantly.
    (cherry picked from commit b608c864ccc59628bd33c033393121b61580460e)

--- dix/window.c
+++ dix/window.c
@@ -3386,7 +3386,7 @@ CheckWindowOptionalNeed (WindowPtr w)
     WindowOptPtr optional;
     WindowOptPtr parentOptional;
 
-    if (!w->parent)
+    if (!w->parent || !w->optional)
 	return;
     optional = w->optional;
     if (optional->dontPropagateMask != DontPropagateMasks[w->dontPropagate])
