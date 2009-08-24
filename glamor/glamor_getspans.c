0360ba361a67f6cfa5c9c65590ed8440a153c16e
Eric Anholt <eric@anholt.net>
2009-08-24 08:06:57 -0700

    glamor: Fix memory leak in getspans.

--- glamor/glamor_getspans.c
+++ glamor/glamor_getspans.c
@@ -58,7 +58,7 @@ glamor_get_spans(DrawablePtr drawable,
 
     switch (drawable->depth) {
     case 1:
-	temp_dst = xalloc(4 * wmax);
+	temp_dst = xalloc(wmax);
 	format = GL_ALPHA;
 	type = GL_UNSIGNED_BYTE;
 	readpixels_dst = temp_dst;
@@ -100,4 +100,5 @@ glamor_get_spans(DrawablePtr drawable,
 	    readpixels_dst += PixmapBytePad(widths[i], drawable->depth);
 	}
     }
+    xfree(temp_dst);
 }
