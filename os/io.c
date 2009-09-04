2122fedb2c058dc27f9df0e856d2193fabe725e2
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-04 11:37:58 +1000

    os: silence bigreqsproto compiler warning
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- os/io.c
+++ os/io.c
@@ -105,7 +105,7 @@ static OsCommPtr AvailableInput = (OsCommPtr)NULL;
 #define get_req_len(req,cli) ((cli)->swapped ? \
 			      lswaps((req)->length) : (req)->length)
 
-#include <X11/extensions/bigreqstr.h>
+#include <X11/extensions/bigreqsproto.h>
 
 #define get_big_req_len(req,cli) ((cli)->swapped ? \
 				  lswapl(((xBigReq *)(req))->length) : \
