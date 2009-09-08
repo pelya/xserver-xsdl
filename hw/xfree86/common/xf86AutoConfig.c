cafa7c5f9017efdc33c348b6a55d80d44df0fe17
Martin-Éric Racine <q-funk@iki.fi>
2009-09-08 23:52:52 +0300

    Added comment with the PCI device ID of Geode SCx200 series.

--- hw/xfree86/common/xf86AutoConfig.c
+++ hw/xfree86/common/xf86AutoConfig.c
@@ -165,7 +165,7 @@ videoPtrToDriverList(struct pci_device *dev,
 		   forked by AMD for GEODE so we keep it as a backup */
 		driverList[1] = "nsc";
 	    } else 
-		/* ... or any other NSC Geode e.g. SC series */
+		/* ... any other NSC Geode SC variant e.g. 0x0504 for SCx200 */
 		driverList[0] = "nsc";
 	    break;
 	/* Cyrix Geode GX1 */
