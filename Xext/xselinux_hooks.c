0ff28319906eeb3f236acd72201c416ce01f2c6e
Eamon Walsh <ewalsh@tycho.nsa.gov>
2009-09-15 19:41:04 -0400

    xselinux: Stop special-casing QueryPointer access checks.
    XACE has been changed to not return BadAccess on device read failures.
    Thus, no need for this workaround code.
    
    Signed-off-by: Eamon Walsh <ewalsh@tycho.nsa.gov>

--- Xext/xselinux_hooks.c
+++ Xext/xselinux_hooks.c
@@ -372,17 +372,6 @@ SELinuxDevice(CallbackListPtr *pcbl, pointer unused, pointer calldata)
 	}
     }
 
-    /* XXX only check read permission on XQueryKeymap */
-    /* This is to allow the numerous apps that call XQueryPointer to work */
-    if (rec->access_mode & DixReadAccess) {
-	ClientPtr client = rec->client;
-	REQUEST(xReq);
-	if (stuff && stuff->reqType != X_QueryKeymap) {
-	    rec->access_mode &= ~DixReadAccess;
-	    rec->access_mode |= DixGetAttrAccess;
-	}
-    }
-
     rc = SELinuxDoCheck(subj, obj, SECCLASS_X_DEVICE, rec->access_mode,
 			&auditdata);
     if (rc != Success)
