pelya/xserver-xsdl

commit 9040dab76182d1a019ca7fef7b29733d2c199e61

xfixes: minor clean ups on createInvisibleCursor()
- unused return value
- no reason to declare static variable given the function is just called
  once
- no reason to declare different type and cast it after.

Signed-off-by: Tiago Vignatti <tiago.vignatti@nokia.com>

xfixes/cursor.c
@@ -1046,12 +1046,11 @@ static CursorPtr
 createInvisibleCursor (void)
 {
     CursorPtr pCursor;
-    static unsigned int *psrcbits, *pmaskbits;
+    unsigned char *psrcbits, *pmaskbits;
     CursorMetricRec cm;
-    int rc;
 
-    psrcbits = (unsigned int *) xalloc(4);
-    pmaskbits = (unsigned int *) xalloc(4);
+    psrcbits = (unsigned char *) xalloc(4);
+    pmaskbits = (unsigned char *) xalloc(4);
     if (psrcbits == NULL || pmaskbits == NULL) {
 	return NULL;
     }
@@ -1063,9 +1062,7 @@ createInvisibleCursor (void)
     cm.xhot = 0;
     cm.yhot = 0;
 
-    rc = AllocARGBCursor(
-	        (unsigned char *)psrcbits,
-		(unsigned char *)pmaskbits,
+    AllocARGBCursor(psrcbits, pmaskbits,
 		NULL, &cm,
 		0, 0, 0,
 		0, 0, 0,

commit 2a890d85e923e1da0331b02e58e95f98b11f5de0

fixes: Make sure invisible cursor is inialized with zero src/mask
This fixes a regression introduced in 9040dab76182d1a019ca7fef7b29733d2c199e61.

Signed-off-by: Thomas Jaeger <ThJaeger@gmail.com>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

xfixes/cursor.c
@@ -1049,13 +1049,11 @@ createInvisibleCursor (void)
     unsigned char *psrcbits, *pmaskbits;
     CursorMetricRec cm;
 
-    psrcbits = (unsigned char *) xalloc(4);
-    pmaskbits = (unsigned char *) xalloc(4);
+    psrcbits = (unsigned char *) xcalloc(4, 1);
+    pmaskbits = (unsigned char *) xcalloc(4, 1);
     if (psrcbits == NULL || pmaskbits == NULL) {
 	return NULL;
     }
-    *psrcbits = 0;
-    *pmaskbits = 0;
 
     cm.width = 1;
     cm.height = 1;