b0dd6be2c8703f7062d45ac9fd646550c7d54e3b
Jamey Sharp <jamey@minilop.net>
2009-10-08 13:38:44 +1100

    Cast small-int values through intptr_t when passed as pointers
    On 64-bit systems, int and pointers don't have the same size, so GCC gives
    warnings about casts between int and pointer types. However, in the cases
    covered by this patch, it's always a value that fits in int being stored
    temporarily as a pointer and then converted back later, which is safe.
    Casting through the pointer-sized integer type intptr_t convinces the
    compiler that this is OK.
    
    Signed-off-by: Jamey Sharp <jamey@minilop.net>
    Signed-off-by: Daniel Stone <daniel@fooishbar.org>

--- dix/events.c
+++ dix/events.c
@@ -1460,7 +1460,7 @@ static DevPrivateKey GrabPrivateKey = &GrabPrivateKeyIndex;
 static void
 DetachFromMaster(DeviceIntPtr dev)
 {
-    int id;
+    intptr_t id;
     if (!dev->u.master)
         return;
 
@@ -1482,7 +1482,7 @@ ReattachToOldMaster(DeviceIntPtr dev)
 
 
     p = dixLookupPrivate(&dev->devPrivates, GrabPrivateKey);
-    id = (int)p; /* silence gcc warnings */
+    id = (intptr_t) p; /* silence gcc warnings */
     dixLookupDevice(&master, id, serverClient, DixUseAccess);
 
     if (master)
--- glx/glxcmds.c
+++ glx/glxcmds.c
@@ -2057,7 +2057,7 @@ int __glXDisp_BindSwapBarrierSGIX(__GLXclientState *cl, GLbyte *pc)
             if (ret == Success) {
                 if (barrier)
                     /* add source for cleanup when drawable is gone */
-                    AddResource(drawable, __glXSwapBarrierRes, (pointer)screen);
+                    AddResource(drawable, __glXSwapBarrierRes, (pointer)(intptr_t)screen);
                 else
                     /* delete source */
                     FreeResourceByType(drawable, __glXSwapBarrierRes, FALSE);
