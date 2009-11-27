66bb8c6fbdfc0fc0d971aac4ec6f949bb9288c1b
Peter Hutterer <peter.hutterer@who-t.net>
2009-11-27 16:20:13 +1000

    dix: remove core devices when shutting down. (#25028)
    NewInputDeviceRequest (and RemoveDevice) have checks in place to not allow
    removal of the VCP/VCK. When shutting down, they need to be cleaned up
    nonetheless to free the memory associated.
    
    X.Org Bug 25028 <http://bugs.freedesktop.org/show_bug.cgi?id=25028>
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Keith Packard <keithp@keithp.com>

--- dix/devices.c
+++ dix/devices.c
@@ -907,6 +907,9 @@ CloseDownDevices(void)
         DeleteInputDeviceRequest(dev);
     }
 
+    CloseDevice(inputInfo.pointer);
+    CloseDevice(inputInfo.keyboard);
+
     inputInfo.devices = NULL;
     inputInfo.off_devices = NULL;
     inputInfo.keyboard = NULL;
