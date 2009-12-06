3fad969a90ccab604359ec4de05e745bb4b909e1
Julien Cristau <jcristau@debian.org>
2009-12-06 20:27:06 +0100

    Move config_init() after CreateWellKnownSockets() and InitCoreDevices()
    config_init() can now add devices directly instead of scheduling a
    timer.
    
    Signed-off-by: Julien Cristau <jcristau@debian.org>
    Reviewed-by: Peter Hutterer <peter.hutterer@who-t.net>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- dix/main.c
+++ dix/main.c
@@ -168,7 +168,6 @@ int main(int argc, char *argv[], char *envp[])
 	InitBlockAndWakeupHandlers();
 	/* Perform any operating system dependent initializations you'd like */
 	OsInit();
-        config_init();
 	if(serverGeneration == 1)
 	{
 	    CreateWellKnownSockets();
@@ -254,6 +253,7 @@ int main(int argc, char *argv[], char *envp[])
 	    InitRootWindow(WindowTable[i]);
 
         InitCoreDevices();
+        config_init();
 	InitInput(argc, argv);
 	InitAndStartDevices();
 
