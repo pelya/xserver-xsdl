dbf249ec6638f0a8dfa4c2286099845aafc8ac88
Peter Hutterer <peter.hutterer@who-t.net>
2010-06-29 10:43:51 +1000

    xkb: remove now obsolete comment.
    Looks like nothing broke from removing the hardcoded CoreProcessPointerEvent
    call. Whoop. Di. Doo.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- xkb/xkbAccessX.c
+++ xkb/xkbAccessX.c
@@ -712,22 +712,6 @@ DeviceEvent     *event = &ev->device_event;
 	changed |= XkbPointerButtonMask;
     }
 
-    /* Guesswork. mostly. 
-     * xkb actuall goes through some effort to transparently wrap the
-     * processInputProcs (see XkbSetExtension). But we all love fun, so the
-     * previous XKB implementation just hardcoded the CPPE call here instead
-     * of unwrapping like anybody with any sense of decency would do. 
-     * I got no clue what the correct thing to do is, but my guess is that
-     * it's not hardcoding. I may be wrong. whatever it is, don't come whining
-     * to me. I just work here. 
-     *
-     * Anyway. here's the old call, if you don't like the wrapping, revert it.
-     *
-     * CoreProcessPointerEvent(xE,mouse,count);
-     *
-     *          see. it's still steaming. told you. (whot)
-     */
-
     UNWRAP_PROCESS_INPUT_PROC(mouse, xkbPrivPtr, backupproc);
     mouse->public.processInputProc(ev, mouse);
     COND_WRAP_PROCESS_INPUT_PROC(mouse, xkbPrivPtr,
