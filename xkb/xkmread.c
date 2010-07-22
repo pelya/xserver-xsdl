2475ef60977f6813dec74ef0837a5915b8a48bbc
Pauli Nieminen <ext-pauli.nieminen@nokia.com>
2010-07-22 12:48:55 +0300

    xkb: Fix NULL pointer dereference
    xkb->names is dereferenced in else path too.
    
    Signed-off-by: Pauli Nieminen <ext-pauli.nieminen@nokia.com>
    Reviewed-by: Peter Hutterer <peter.hutterer@who-t.net>

--- xkb/xkmread.c
+++ xkb/xkmread.c
@@ -686,7 +686,11 @@ int			nRead=0;
 	    if ((tmp=XkmGetCountedString(file,buf,100))<1)
 		return -1;
 	    nRead+= tmp;
-	    if ((buf[0]!='\0')&&(xkb->names)) {
+
+	    if (!xkb->names)
+		    continue;
+
+	    if (buf[0]!='\0') {
 		Atom name;
 		name= XkbInternAtom(buf,0);
 		xkb->names->groups[i]= name;
