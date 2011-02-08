85f9017393c9bb19553e9afcf554673a44a09993
Ander Conselvan de Oliveira <ander.conselvan-de-oliveira@nokia.com>
2011-02-08 11:10:10 +0200

    ProcXkbGetXkbByName: fix use of uninitialised bytes valgrind error.
    ==9999== Syscall param writev(vector[...]) points to uninitialised byte(s)
    ==9999==    at 0x4AB5154: writev (writev.c:51)
    ==9999==    by 0x7C7C3: _XSERVTransWritev (Xtrans.c:912)
    ==9999==    by 0x61C8B: FlushClient (io.c:924)
    ==9999==    by 0x62423: WriteToClient (io.c:846)
    ==9999==    by 0xCE39B: XkbSendMap (xkb.c:1408)
    ==9999==    by 0xD247B: ProcXkbGetKbdByName (xkb.c:5814)
    ==9999==    by 0x4AB53: Dispatch (dispatch.c:432)
    ==9999==    by 0x205BF: main (main.c:291)
    ==9999==  Address 0x557eb68 is 40 bytes inside a block of size 4,096 alloc'd
    ==9999==    at 0x48334A4: calloc (vg_replace_malloc.c:467)
    ==9999==    by 0x62567: WriteToClient (io.c:1065)
    ==9999==    by 0x452EB: ProcEstablishConnection (dispatch.c:3685)
    ==9999==    by 0x4AB53: Dispatch (dispatch.c:432)
    ==9999==    by 0x205BF: main (main.c:291)
    ==9999==  Uninitialised value was created by a stack allocation
    ==9999==    at 0xD1910: ProcXkbGetKbdByName (xkb.c:5559)
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Oliver McFadden <oliver.mcfadden@nokia.com>
    Signed-off-by: Ander Conselvan de Oliveira <ander.conselvan-de-oliveira@nokia.com>

--- xkb/xkb.c
+++ xkb/xkb.c
@@ -5569,13 +5569,13 @@ ProcXkbGetKbdByName(ClientPtr client)
 {
     DeviceIntPtr 		dev;
     DeviceIntPtr                tmpd;
-    xkbGetKbdByNameReply 	rep;
-    xkbGetMapReply		mrep;
-    xkbGetCompatMapReply	crep;
-    xkbGetIndicatorMapReply	irep;
-    xkbGetNamesReply		nrep;
-    xkbGetGeometryReply		grep;
-    XkbComponentNamesRec	names;
+    xkbGetKbdByNameReply 	rep = {0};
+    xkbGetMapReply		mrep = {0};
+    xkbGetCompatMapReply	crep = {0};
+    xkbGetIndicatorMapReply	irep = {0};
+    xkbGetNamesReply		nrep = {0};
+    xkbGetGeometryReply		grep = {0};
+    XkbComponentNamesRec	names = {0};
     XkbDescPtr			xkb, new;
     unsigned char *		str;
     char 			mapFile[PATH_MAX];
