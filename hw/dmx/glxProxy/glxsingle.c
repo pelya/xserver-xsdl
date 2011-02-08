f928caca6e60215bc9ab423acae5542dfabc9bec
Adam Jackson <ajax@redhat.com>
2011-02-08 14:45:31 -0500

    glxproxy: warning fix
    glxsingle.c: In function ‘__glXForwardAllWithReply’:
    glxsingle.c:300:10: warning: ‘be_buf’ may be used uninitialized in this function
    glxsingle.c:301:10: warning: ‘be_buf_size’ may be used uninitialized in this function
    
    Reviewed-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Signed-off-by: Adam Jackson <ajax@redhat.com>

--- hw/dmx/glxProxy/glxsingle.c
+++ hw/dmx/glxProxy/glxsingle.c
@@ -297,8 +297,8 @@ int __glXForwardAllWithReply( __GLXclientState *cl, GLbyte *pc )
    xGLXSingleReply be_reply;
     __GLXcontext *glxc;
    int buf_size;
-   char *be_buf;
-   int   be_buf_size;
+   char *be_buf = NULL;
+   int   be_buf_size = 0;
    int from_screen = 0;
    int to_screen = 0;
    int s;
