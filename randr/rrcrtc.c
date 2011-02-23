4d91e7a63161cdde9af53aedb3fe3e53dbe3049e
Keith Packard <keithp@keithp.com>
2011-02-23 11:17:20 -0800

    Revert "ProcRRSetCrtcConfigs uses 'configs' without being initialized"
    This reverts commit b0f4bd61f0caf80f3be9a176f1f7a707bc6628d8.

--- randr/rrcrtc.c
+++ randr/rrcrtc.c
@@ -1682,9 +1682,9 @@ ProcRRSetCrtcConfigs (ClientPtr client)
     rrScrPrivPtr	    scr_priv;
     xRRCrtcConfig	    *x_configs;
     RRScreenConfigRec	    screen_config;
-    RRCrtcConfigPtr	    configs = NULL;
+    RRCrtcConfigPtr	    configs;
     RROutput		    *output_ids;
-    int			    num_configs = 0;
+    int			    num_configs;
     int			    rc, i;
     int			    extra_len;
     int			    num_output_ids;
