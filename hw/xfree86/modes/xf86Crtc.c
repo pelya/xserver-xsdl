793a242c89fccdbf1a2557ba33da1da2cb4ff8c4
Keith Packard <keithp@keithp.com>
2011-02-23 11:17:33 -0800

    Revert "DIX is responsible for ref counting scanout pixmaps."
    This reverts commit 96b4d4787bf82edd9d06eb9a6e94bc45412c7df2.

--- hw/xfree86/modes/xf86Crtc.c
+++ hw/xfree86/modes/xf86Crtc.c
@@ -414,6 +414,10 @@ done:
 	crtc->active = TRUE;
 	if (scrn->pScreen)
 	    xf86CrtcSetScreenSubpixelOrder (scrn->pScreen);
+	if (crtc->scanoutPixmap)
+	    ++crtc->scanoutPixmap->refcnt;
+	if (saved_scanout_pixmap)
+	    (*scrn->pScreen->DestroyPixmap)(saved_scanout_pixmap);
 	if (scrn->ModeSet)
 	    scrn->ModeSet(scrn);
     } else {
