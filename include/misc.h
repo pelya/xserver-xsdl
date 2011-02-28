51f353d0a0d116af16d7d9590cadef6c56328746
Adam Jackson <ajax@redhat.com>
2011-02-28 13:10:20 -0500

    dix: Fix ATOM typedef
    unsigned long is needlessly large on LP64.  Use uint32_t instead.
    
    Reviewed-by: Daniel Stone <daniel@fooishbar.org>
    Signed-off-by: Adam Jackson <ajax@redhat.com>

--- include/misc.h
+++ include/misc.h
@@ -79,6 +79,7 @@ OF THIS SOFTWARE.
 #include <X11/Xdefs.h>
 
 #include <stddef.h>
+#include <stdint.h>
 
 #ifndef MAXSCREENS
 #define MAXSCREENS	16
@@ -91,7 +92,7 @@ OF THIS SOFTWARE.
 #define EXTENSION_EVENT_BASE 64
 #define EXTENSION_BASE 128
 
-typedef unsigned long ATOM;
+typedef uint32_t ATOM;
 
 #ifndef TRUE
 #define TRUE 1
