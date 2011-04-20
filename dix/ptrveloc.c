8c528c667da7c7fba0ba07973a06b2d4613fbf70
Peter Hutterer <peter.hutterer@who-t.net>
2011-04-20 13:14:11 +1000

    dix: fix typo in direction calculation
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Daniel Stone <daniel@fooishbar.org>
    Reviewed-by: Jamey Sharp <jamey@minilop.net>
    Reviewed-by: Simon Thum <simon.thum@gmx.de>

--- dix/ptrveloc.c
+++ dix/ptrveloc.c
@@ -443,7 +443,7 @@ DoGetDirection(int dx, int dy){
     float r;
     int i1, i2;
     /* on insignificant mickeys, flag 135 degrees */
-    if(abs(dx) < 2 && abs(dy < 2)){
+    if(abs(dx) < 2 && abs(dy) < 2){
 	/* first check diagonal cases */
 	if(dx > 0 && dy > 0)
 	    return 4+8+16;
