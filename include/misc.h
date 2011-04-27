9eab5b3443a1926a29385948acc6c5e0843465ea
Matt Turner <mattst88@gmail.com>
2011-04-27 18:25:27 -0400

    Replace Fabs() macro with fabs() function
    gcc generates better code with fabs() anyway.
    
    Reviewed-by: Jeremy Huddleston <jeremyhu@apple.com>
    Tested-by: Jeremy Huddleston <jeremyhu@apple.com>
    Reviewed-by: Daniel Stone <daniel@fooishbar.org>
    Signed-off-by: Matt Turner <mattst88@gmail.com>

--- include/misc.h
+++ include/misc.h
@@ -139,9 +139,6 @@ typedef struct _xReq *xReqPtr;
  * it in case we haven't done that yet.
  */
 #include <stdlib.h>
-#ifndef Fabs
-#define Fabs(a) ((a) > 0.0 ? (a) : -(a))	/* floating absolute value */
-#endif
 #define sign(x) ((x) < 0 ? -1 : ((x) > 0 ? 1 : 0))
 /* this assumes b > 0 */
 #define modulus(a, b, d)    if (((d) = (a) % (b)) < 0) (d) += (b)
