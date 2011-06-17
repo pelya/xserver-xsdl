b27d61e4433a2a0140ed8a6128e4427451e37fec
Matěj Cepl <mcepl@redhat.com>
2011-06-17 15:26:17 +0100

    Fix UTF-8 encoding
    Report to find out all non-UTF-8 files created by
    
    cat extensions |xargs -I XXXX find . -name \*.XXXX |while read FILE ; do
        if ( iconv -f utf8 -t ucs2 $FILE >/dev/null 2>/dev/null ) ; then
            /bin/true
        else
            echo $FILE
        fi
    done >>report
    
    Signed-off-by: Matěj Cepl <mcepl@redhat.com>
    Reviewed-by: Daniel Stone <daniel@fooishbar.org>
    
    [Daniel: git am failed for me, so I redid it.  The method listed in the
             commit message also failed, so I just used file/grep/iconv.  The
             results are the same though.]
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- hw/kdrive/src/kinput.c
+++ hw/kdrive/src/kinput.c
@@ -1,6 +1,6 @@
 /*
- * Copyright © 1999 Keith Packard
- * Copyright © 2006 Nokia Corporation
+ * Copyright Â© 1999 Keith Packard
+ * Copyright Â© 2006 Nokia Corporation
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
