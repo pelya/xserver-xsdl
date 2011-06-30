3d3114d55a2a323f8d49c3549a0dfdf9d4acf89d
Jon TURNEY <jon.turney@dronecode.org.uk>
2011-06-30 14:19:01 +0100

    hw/xwin: Handle the virtual key code generated by the Fn key on IBM Lenovo laptops
    Apparently, IBM Leonovo laptops can generate a key-press event for the Fn
    key, with virtual key code 0xFF and scan code extended 0x63
    
    Handle this specially, rather than just passing on key code 0x63 (delete),
    so you don't delete what you just typed when you adjust the screen brightness,
    etc. :-)
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>
    Reviewed-by: Colin Harrison <colin.harrison@virgin.net>

--- hw/xwin/winkeybd.h
+++ hw/xwin/winkeybd.h
@@ -35,6 +35,8 @@
  */
 #include "winkeynames.h"
 
+#define VK_FN 0xFF
+
 #define		WIN_KEYMAP_COLS		3
 
 /* Rows 160 through 165 correspond to software-generated codes, which
@@ -298,7 +300,7 @@ g_iKeyMap [] = {
   /* 252 */	0,		0,		0,
   /* 253 */	0,		0,		0,
   /* 254 */	0,		0,		0,
-  /* 255 */	0,		0,		0
+  /* 255 */	VK_FN,		0,		KEY_Fn  /* Most keyboards don't generate a scancode for Fn, but a few do... */
 };
 
 #endif /* WINKEYBD_H */
