f1d75f3b742231c4c09f13d7a22ed920014cb5ef
Pierre-Loup A. Griffais <pgriffais@nvidia.com>
2011-07-28 15:17:59 -0700

    Revert "composite: Don't backfill non-MapWindow allocations"
    This reverts commit db8840600e8e21356241eb87395031388d9b54d2.
    
    It was an optimization for the resize case, but 193ecc8b453b22 made
    it so that no backfilling takes place on resize if left in.
    
    Signed-off-by: Pierre-Loup A. Griffais <pgriffais@nvidia.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    
    Conflicts:
    
    	composite/compalloc.c
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- composite/compalloc.c
+++ composite/compalloc.c
@@ -557,7 +557,7 @@ compUnredirectOneSubwindow (WindowPtr pParent, WindowPtr pWin)
 }
 
 static PixmapPtr
-compNewPixmap (WindowPtr pWin, int x, int y, int w, int h, Bool map)
+compNewPixmap (WindowPtr pWin, int x, int y, int w, int h)
 {
     ScreenPtr	    pScreen = pWin->drawable.pScreen;
     WindowPtr	    pParent = pWin->parent;
@@ -571,10 +571,6 @@ compNewPixmap (WindowPtr pWin, int x, int y, int w, int h, Bool map)
     
     pPixmap->screen_x = x;
     pPixmap->screen_y = y;
-
-    /* resize allocations will update later in compCopyWindow, not here */
-    if (!map)
-	return pPixmap;
 
     if (pParent->drawable.depth == pWin->drawable.depth)
     {
@@ -641,7 +637,7 @@ compAllocPixmap (WindowPtr pWin)
     int		    y = pWin->drawable.y - bw;
     int		    w = pWin->drawable.width + (bw << 1);
     int		    h = pWin->drawable.height + (bw << 1);
-    PixmapPtr	    pPixmap = compNewPixmap (pWin, x, y, w, h, TRUE);
+    PixmapPtr	    pPixmap = compNewPixmap (pWin, x, y, w, h);
     CompWindowPtr   cw = GetCompWindow (pWin);
 
     if (!pPixmap)
@@ -713,7 +709,7 @@ compReallocPixmap (WindowPtr pWin, int draw_x, int draw_y,
     pix_h = h + (bw << 1);
     if (pix_w != pOld->drawable.width || pix_h != pOld->drawable.height)
     {
-	pNew = compNewPixmap (pWin, pix_x, pix_y, pix_w, pix_h, FALSE);
+	pNew = compNewPixmap (pWin, pix_x, pix_y, pix_w, pix_h);
 	if (!pNew)
 	    return FALSE;
 	cw->pOldPixmap = pOld;
