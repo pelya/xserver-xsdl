e189dbb3e57d30eb96034d4ce9544ce7a93a371e
Alan Coopersmith <alan.coopersmith@oracle.com>
2011-10-28 20:19:44 -0700

    Convert AllocXTestDevice to use asprintf
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Jeremy Huddleston <jeremyhu@apple.com>

--- Xext/xtest.c
+++ Xext/xtest.c
@@ -608,12 +608,11 @@ int AllocXTestDevice (ClientPtr client, char* name,
                      DeviceIntPtr master_ptr, DeviceIntPtr master_keybd)
 {
     int retval;
-    int len = strlen(name);
-    char *xtestname = calloc(len + 7, 1 );
+    char *xtestname;
     char dummy = 1;
 
-    strncpy( xtestname, name, len);
-    strncat( xtestname, " XTEST", 6 );
+    if (asprintf(&xtestname, "%s XTEST", name) == -1)
+	return BadAlloc;
 
     retval = AllocDevicePair( client, xtestname, ptr, keybd, CorePointerProc, CoreKeyboardProc, FALSE);
     if ( retval == Success ){
