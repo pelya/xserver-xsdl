20efd3c15829d8fbb3610d5af41b67a627e63d21
Peter Hutterer <peter.hutterer@who-t.net>
2011-11-29 16:15:37 +1000

    Xi: when removing a device, reset ClientPointers where needed
    if a client had the to-be-removed device as ClientPointer, reset to NULL.
    
    Fixes #43165
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Jeremy Huddleston <jeremyhu@apple.com>
    (cherry picked from commit d2ebbcdaf6b13d70eee704b1764ff349e1be22a0)

--- Xi/xichangehierarchy.c
+++ Xi/xichangehierarchy.c
@@ -203,6 +203,19 @@ unwind:
 }
 
 static int
+disable_clientpointer(DeviceIntPtr dev)
+{
+    int i;
+
+    for (i = 0; i < currentMaxClients; i++)
+    {
+        ClientPtr client = clients[i];
+        if (client && client->clientPtr == dev)
+            client->clientPtr = NULL;
+    }
+}
+
+static int
 remove_master(ClientPtr client, xXIRemoveMasterInfo *r,
               int flags[MAXDEVICES])
 {
@@ -251,6 +264,8 @@ remove_master(ClientPtr client, xXIRemoveMasterInfo *r,
                          DixDestroyAccess);
     if (rc != Success)
         goto unwind;
+
+    disable_clientpointer(ptr);
 
     /* Disabling sends the devices floating, reattach them if
      * desired. */
