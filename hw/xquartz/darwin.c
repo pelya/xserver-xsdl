87939bf8311c0e7d168741fe57bc8fac414811d8
Jeremy Huddleston <jeremyhu@apple.com>
2012-03-13 00:25:53 -0700

    XQuartz: Move our logs into an X11 subdirectory
    Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>

--- hw/xquartz/darwin.c
+++ hw/xquartz/darwin.c
@@ -56,6 +56,7 @@
 
 #include <sys/types.h>
 #include <sys/time.h>
+#include <sys/stat.h>
 #include <sys/syslimits.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -650,9 +651,14 @@ OsVendorInit(void)
         char *home = getenv("HOME");
 
         assert(home);
-        assert(0 <
-               asprintf(&lf, "%s/Library/Logs/%s.X11.log", home,
-                        bundle_id_prefix));
+
+        /* Ignore errors.  If EEXIST, we don't care.  If anything else,
+         * LogInit will handle it for us.
+         */
+        (void)mkdir(lf, S_IRWXU | S_IRWXG | S_IRWXO);
+        free(lf);
+
+        assert(0 < asprintf(&lf, "%s/Library/Logs/X11/%s.log", home, bundle_id_prefix));
         LogInit(lf, ".old");
         free(lf);
 
