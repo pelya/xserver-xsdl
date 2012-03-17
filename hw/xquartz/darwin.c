cd84c0949a9f397d8b803d3b87c9d02b260d4795
Jeremy Huddleston <jeremyhu@apple.com>
2012-03-17 00:08:19 -0700

    XQuartz: Xi: darwinPointer is now Relative
    There is really no real reason why this should be necessary, but wine
    developers are stuborn, so doing this to try to work around this wine
    issue:
    
    http://bugs.winehq.org/show_bug.cgi?id=29732
    
    Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>

--- hw/xquartz/darwin.c
+++ hw/xquartz/darwin.c
@@ -336,9 +336,9 @@ DarwinMouseProc(DeviceIntPtr pPointer, int what)
                                 (PtrCtrlProcPtr) NoopDDA,
                                 GetMotionHistorySize(), NAXES, axes_labels);
         InitValuatorAxisStruct(pPointer, 0, axes_labels[0], NO_AXIS_LIMITS,
-                               NO_AXIS_LIMITS, 0, 0, 0, Absolute);
+                               NO_AXIS_LIMITS, 1, 0, 1, Relative);
         InitValuatorAxisStruct(pPointer, 1, axes_labels[1], NO_AXIS_LIMITS,
-                               NO_AXIS_LIMITS, 0, 0, 0, Absolute);
+                               NO_AXIS_LIMITS, 1, 0, 1, Relative);
         break;
     case DEVICE_ON:
         pPointer->public.on = TRUE;
