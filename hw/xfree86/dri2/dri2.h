b8a3267c36e2e335b888bd4f2ef2f2c477cdfdce
Daniel Stone <daniel@fooishbar.org>
2012-07-10 02:03:17 +0100

    DRI2: Remove prototype for DRI2DestroyDrawable
    DRI2DestroyDrawable() was still being _X_EXPORTed, but hasn't existed
    since 1da1f33f last year.
    
    Signed-off-by: Daniel Stone <daniel@fooishbar.org>
    Reviewed-by: Cyril Brulebois <kibi@debian.org>
    Reviewed-by: Jamey Sharp <jamey@minilop.net>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- hw/xfree86/dri2/dri2.h
+++ hw/xfree86/dri2/dri2.h
@@ -276,8 +276,6 @@ extern _X_EXPORT int DRI2CreateDrawable(ClientPtr client,
                                         DRI2InvalidateProcPtr invalidate,
                                         void *priv);
 
-extern _X_EXPORT void DRI2DestroyDrawable(DrawablePtr pDraw);
-
 extern _X_EXPORT DRI2BufferPtr *DRI2GetBuffers(DrawablePtr pDraw,
                                                int *width,
                                                int *height,
