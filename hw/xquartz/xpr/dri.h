pelya/xserver-xsdl

commit a1d41e311c21eb6627caa0d168e070ceaf90806f

Move extension initialisation prototypes into extinit.h
Create extinit.h (and xf86Extensions.h, for Xorg-specific extensions) to
hold all our extension initialisation prototypes, rather than
duplicating them everywhere.

Signed-off-by: Daniel Stone <daniel@fooishbar.org>
Reviewed-by: Peter Hutterer <peter.hutterer@who-t.net>
Signed-off-by: Keith Packard <keithp@keithp.com>

hw/xquartz/xpr/dri.h
@@ -72,9 +72,6 @@ DRIFinishScreenInit(ScreenPtr pScreen);
 extern void
 DRICloseScreen(ScreenPtr pScreen);
 
-extern Bool
-DRIExtensionInit(void);
-
 extern void
 DRIReset(void);
 

commit 1720cd9badf21e5d6b2c6c929e609ab3b0b92fd2

XQuartz: Re-add prototype for DRIExtensionInit
Regression from: a1d41e311c21eb6627caa0d168e070ceaf90806f

Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>
CC: Daniel Stone <daniel@fooishbar.org>

hw/xquartz/xpr/dri.h
@@ -72,6 +72,9 @@ DRIFinishScreenInit(ScreenPtr pScreen);
 extern void
 DRICloseScreen(ScreenPtr pScreen);
 
+extern Bool
+DRIExtensionInit(void);
+
 extern void
 DRIReset(void);
 