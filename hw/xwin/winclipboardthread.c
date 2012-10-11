0e85e5e62819c9e516d6e19836f9f5d7a5d53e87
Colin Harrison <colin.harrison@virgin.net>
2012-10-11 18:05:47 +0100

    hw/xwin: Fix no return value warning in winClipboardProc()
    winclipboardthread.c: In function ‘winClipboardProc’:
    winclipboardthread.c:415:9: warning: ‘return’ with no value, in function returning non-void
    winclipboardthread.c:424:13: warning: ‘return’ with no value, in function returning non-void
    
    Signed-off-by: Colin Harrison <colin.harrison@virgin.net>
    Reviewed-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- hw/xwin/winclipboardthread.c
+++ hw/xwin/winclipboardthread.c
@@ -412,7 +412,7 @@ winClipboardProc(void *pvNotUsed)
             ("winClipboardProc - the clipboard thread has restarted %d times and seems to be unstable, disabling clipboard integration\n",
              clipboardRestarts);
         g_fClipboard = FALSE;
-        return;
+        return NULL;
     }
 
     if (g_fClipboard) {
@@ -421,7 +421,7 @@ winClipboardProc(void *pvNotUsed)
         /* Create the clipboard client thread */
         if (!winInitClipboard()) {
             ErrorF("winClipboardProc - winClipboardInit failed.\n");
-            return;
+            return NULL;
         }
 
         winDebug("winClipboardProc - winInitClipboard returned.\n");
