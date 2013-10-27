12e14b123162a8c978265587b8d17930f747f351
pelya <x.pelya.x@gmail.com>
2013-10-28 01:11:18 +0200

    Minor fixes

--- android/build.sh
+++ android/build.sh
@@ -2,8 +2,29 @@
 
 set -x
 
-NCPU=4
+NCPU=8
 export BUILDDIR=`pwd`
+NDK=`which ndk-build`
+NDK=`dirname $NDK`
+NDK=`readlink -f $NDK`
+
+
+[ -e pixman-0.30.2/libpixman.so ] || {
+[ -e pixman-0.30.2 ] || curl http://cairographics.org/releases/pixman-0.30.2.tar.gz | tar xvz || exit 1
+cd pixman-0.30.2
+
+env CFLAGS="-I`pwd`/../../../sdl/project/jni/png/include -I$NDK/sources/android/cpufeatures" \
+../setCrossEnvironment.sh \
+./configure \
+--host=arm-linux-androideabi \
+
+../setCrossEnvironment.sh \
+nice make -j$NCPU V=1 2>&1 || exit 1
+
+cd $BUILDDIR
+}
+
+
 
 [ -e Makefile ] || \
 env CFLAGS="-include `pwd`/compiler-workarounds.h" \
@@ -11,7 +32,6 @@ env CFLAGS="-include `pwd`/compiler-workarounds.h" \
 ../configure \
 --host=arm-linux-androideabi \
 --prefix=/usr \
---enable-debug \
 --disable-xorg --disable-dmx --disable-xvfb --disable-xnest --disable-xquartz --disable-xwin \
 --disable-xephyr --disable-xfake --disable-xfbdev --disable-unit-tests --disable-tslib \
 --disable-shm --disable-mitshm --disable-dri --disable-dri2 --disable-glx --disable-xf86vidmode \
@@ -21,4 +41,4 @@ env CFLAGS="-include `pwd`/compiler-workarounds.h" \
 ln -sf /usr/include/X11 include/X11
 ln -sf ../../../sdl/project/jni/sdl-1.2/include include/SDL
 
-{ ./setCrossEnvironment.sh nice -n19 make -j8 V=1 2>&1 || exit 1 ; } | tee build.log
+{ ./setCrossEnvironment.sh nice -n19 make -j$NCPU V=1 2>&1 || exit 1 ; } | tee build.log
