b4248f116f8a07ea2c66df44e96fa6f0a9de78ba
pelya <x.pelya.x@gmail.com>
2013-10-27 22:51:09 +0200

    Fixes to input events

---
+++ conf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+env SDL=yes CFLAGS="-O0 -g" \
+./configure --prefix=/usr \
+--enable-debug \
+--disable-xorg --disable-dmx --disable-xvfb --disable-xnest --disable-xquartz --disable-xwin \
+--disable-xephyr --disable-xfake --disable-xfbdev --disable-unit-tests \
+--disable-shm --disable-mitshm --disable-dri --disable-dri2 --disable-glx --disable-xf86vidmode \
+--enable-xsdl --enable-kdrive --enable-kdrive-kbd --enable-kdrive-mouse --enable-kdrive-evdev \
+
+nice -n19 make -j8
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./Xsdl :1111 -screen 640x480x24 # -mouse mouse -keybd keyboard
