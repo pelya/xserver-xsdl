73926622b91fde01148727f26d6aad5e6827c1d2
Peter Hutterer <peter.hutterer@who-t.net>
2014-01-24 18:33:54 +1000

    include: make the various button array lengths more obvious
    No functional changes, just making a better case for why MAP_LENGTH is 256.
    "But can't we remove MAP_LENGTH then?" I hear you say? "Why, yes. Go for it!"
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Daniel Stone <daniel@fooishbar.org>

--- include/input.h
+++ include/input.h
@@ -95,8 +95,8 @@ SOFTWARE.
 
 #define NO_AXIS_LIMITS -1
 
-#define MAP_LENGTH	256
-#define DOWN_LENGTH	32      /* 256/8 => number of bytes to hold 256 bits */
+#define MAP_LENGTH	MAX_BUTTONS
+#define DOWN_LENGTH	(MAX_BUTTONS/8)      /* 256/8 => number of bytes to hold 256 bits */
 #define NullGrab ((GrabPtr)NULL)
 #define PointerRootWin ((WindowPtr)PointerRoot)
 #define NoneWin ((WindowPtr)None)
