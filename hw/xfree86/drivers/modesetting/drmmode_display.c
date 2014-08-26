a10ea01475c6d0795b1a00cfd8be9912f4b54235
Eric Anholt <eric@anholt.net>
2014-08-26 11:24:31 -0700

    modesetting: xserver already checks XEXTPROTO >= 7.2.99.901.
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Keith Packard <keithp@keithp.com>

--- hw/xfree86/drivers/modesetting/drmmode_display.c
+++ hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -45,13 +45,7 @@
 
 #include <cursorstr.h>
 
-/* DPMS */
-#ifdef HAVE_XEXTPROTO_71
 #include <X11/extensions/dpmsconst.h>
-#else
-#define DPMS_SERVER
-#include <X11/extensions/dpms.h>
-#endif
 #include "compat-api.h"
 
 #include "driver.h"
