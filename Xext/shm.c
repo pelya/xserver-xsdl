25315ee555dfeb96d9ed83064b1022ed852b612e
Peter Hutterer <peter.hutterer@who-t.net>
2014-10-31 09:16:28 +1000

    Xext: fix clang compiler warning
    shm.c:1150:24: warning: equality comparison with extraneous parentheses
    [-Wparentheses-equality]
        if ((shmdesc->addr == ((char *) -1))) {
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- Xext/shm.c
+++ Xext/shm.c
@@ -1147,7 +1147,7 @@ ProcShmAttachFd(ClientPtr client)
                          fd, 0);
 
     close(fd);
-    if ((shmdesc->addr == ((char *) -1))) {
+    if (shmdesc->addr == ((char *) -1)) {
         free(shmdesc);
         return BadAccess;
     }
@@ -1237,7 +1237,7 @@ ProcShmCreateSegment(ClientPtr client)
                          MAP_SHARED,
                          fd, 0);
 
-    if ((shmdesc->addr == ((char *) -1))) {
+    if (shmdesc->addr == ((char *) -1)) {
         close(fd);
         free(shmdesc);
         return BadAccess;
