fa62ca68ef8be7f63cd0b899dfae3366ca9350cf
Rui Matos <tiagomatos@gmail.com>
2015-04-25 21:19:25 +0200

    dix/events: Set currentTime to the given time stamp in NoticeTime
    The refactoring in commit efc1035ca958f2c9d266338a308518a0834b1773
    removed the actual update of currentTime.
    
    Signed-off-by: Rui Matos <tiagomatos@gmail.com>
    Acked-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Keith Packard <keithp@keithp.com>
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- dix/events.c
+++ dix/events.c
@@ -1057,6 +1057,7 @@ MonthChangedOrBadTime(CARD32 *ms)
 void
 NoticeTime(const DeviceIntPtr dev, TimeStamp time)
 {
+    currentTime = time;
     lastDeviceEventTime[XIAllDevices].time = currentTime;
     lastDeviceEventTime[dev->id].time = currentTime;
 
