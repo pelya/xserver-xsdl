0b9c32489131a5723bd78decf5d2557b94207cf4
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2015-06-09 23:29:40 -0700

    XQuartz: Silence -Wpointer-bool-conversion
    X11Controller.m:417:17: error: address of function 'asl_log_descriptor' will always evaluate to 'true'
          [-Werror,-Wpointer-bool-conversion,Value Conversion Issue]
                if (asl_log_descriptor) {
                ~~  ^~~~~~~~~~~~~~~~~~
    X11Controller.m:417:17: note: prefix with the address-of operator to silence this warning [Semantic Issue]
                if (asl_log_descriptor) {
                    ^
                    &
    
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- hw/xquartz/X11Controller.m
+++ hw/xquartz/X11Controller.m
@@ -364,7 +364,7 @@ extern char *bundle_id_prefix;
     }
 
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
-    if (asl_log_descriptor) {
+    if (&asl_log_descriptor) {
         char *asl_sender;
         aslmsg amsg = asl_new(ASL_TYPE_MSG);
         assert(amsg);
@@ -414,7 +414,7 @@ extern char *bundle_id_prefix;
 
         case 0:                                     /* child2 */
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
-            if (asl_log_descriptor) {
+            if (&asl_log_descriptor) {
                 /* Replace our stdout/stderr */
                 dup2(stdout_pipe[1], STDOUT_FILENO);
                 dup2(stderr_pipe[1], STDERR_FILENO);
@@ -443,7 +443,7 @@ extern char *bundle_id_prefix;
     }
 
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
-    if (asl_log_descriptor) {
+    if (&asl_log_descriptor) {
         /* Close the write ends of the pipe */
         close(stdout_pipe[1]);
         close(stderr_pipe[1]);
