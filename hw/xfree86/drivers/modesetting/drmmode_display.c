45c83a266d18eb515313aa3f1a4d7ff6af53be5d
Daniel Martin <consume.noise@gmail.com>
2015-10-29 14:58:10 +0100

    modesetting: Free output_ids in drmmode_set_mode_major()
    We calloc() output_ids. Let's free() it, too.
    
    Signed-off-by: Daniel Martin <consume.noise@gmail.com>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- hw/xfree86/drivers/modesetting/drmmode_display.c
+++ hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -340,7 +340,7 @@ drmmode_set_mode_major(xf86CrtcPtr crtc, DisplayModePtr mode,
     int saved_x, saved_y;
     Rotation saved_rotation;
     DisplayModeRec saved_mode;
-    uint32_t *output_ids;
+    uint32_t *output_ids = NULL;
     int output_count = 0;
     Bool ret = TRUE;
     int i;
@@ -467,6 +467,8 @@ drmmode_set_mode_major(xf86CrtcPtr crtc, DisplayModePtr mode,
     else
         crtc->active = TRUE;
 #endif
+
+    free(output_ids);
 
     return ret;
 }
