445271ec00f25405fa5ed0e149dd4a9d9f6098d4
Hans de Goede <hdegoede@redhat.com>
2016-07-01 17:36:02 +0200

    glx: Always enable EXT_texture_from_pixmap for DRI swrast glx
    Prior to commit f95645c6f700 ("glx: Don't enable EXT_texture_from_pixmap
    unconditionally") DRI glx would always advertise EXT_texture_from_pixmap.
    
    That commit moved the setting of the extension in the extension bits from
    __glXInitExtensionEnableBits to its callers so that
    __glXInitExtensionEnableBits can be used more generally, but at the same
    time made the setting of EXT_texture_from_pixmap conditionally on
    __DRI_TEX_BUFFER being present.
    
    This has result in an unintended behavior change which breaks e.g.
    compositors running on llvmpipe. This commit makes the DRI swrast glx code
    advertise EXT_texture_from_pixmap unconditionally again fixing this.
    
    Fixes: f95645c6f700 ("glx: Don't enable EXT_texture_from_pixmap unconditionally")
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    ---
    Changes in v2:
    -Only add unconditional advertising of GLX_EXT_texture_from_pixmap
     to glxdriswrast.c, do not also add it to glxdri2.c

--- glx/glxdriswrast.c
+++ glx/glxdriswrast.c
@@ -396,6 +396,7 @@ initializeExtensions(__GLXscreen * screen)
     __glXEnableExtension(screen->glx_enable_bits, "GLX_EXT_framebuffer_sRGB");
     __glXEnableExtension(screen->glx_enable_bits, "GLX_ARB_fbconfig_float");
     __glXEnableExtension(screen->glx_enable_bits, "GLX_EXT_fbconfig_packed_float");
+    __glXEnableExtension(screen->glx_enable_bits, "GLX_EXT_texture_from_pixmap");
 
     extensions = dri->core->getExtensions(dri->driScreen);
 
@@ -407,8 +408,6 @@ initializeExtensions(__GLXscreen * screen)
 
         if (strcmp(extensions[i]->name, __DRI_TEX_BUFFER) == 0) {
             dri->texBuffer = (const __DRItexBufferExtension *) extensions[i];
-            __glXEnableExtension(screen->glx_enable_bits,
-                                 "GLX_EXT_texture_from_pixmap\n");
         }
 
 #ifdef __DRI2_FLUSH_CONTROL
