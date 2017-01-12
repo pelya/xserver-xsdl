23f6dbc96e47be6cbeed78cc9ef303987c3e29a9
Mihail Konev <k.mvc@ya.ru>
2017-01-12 13:21:11 +0500

    tests: Move test/{xi1,xi2}/tests to test/
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Signed-off-by: Mihail Konev <k.mvc@ya.ru>

--- test/tests.c
+++ test/tests.c
@@ -21,5 +21,21 @@ main(int argc, char **argv)
     run_test(xkb_test);
     run_test(xtest_test);
 
+#ifdef LDWRAP_TESTS
+    run_test(protocol_xchangedevicecontrol_test);
+
+    run_test(protocol_xiqueryversion_test);
+    run_test(protocol_xiquerydevice_test);
+    run_test(protocol_xiselectevents_test);
+    run_test(protocol_xigetselectedevents_test);
+    run_test(protocol_xisetclientpointer_test);
+    run_test(protocol_xigetclientpointer_test);
+    run_test(protocol_xipassivegrabdevice_test);
+    run_test(protocol_xiquerypointer_test);
+    run_test(protocol_xiwarppointer_test);
+    run_test(protocol_eventconvert_test);
+    run_test(xi2_test);
+#endif
+
     return 0;
 }
