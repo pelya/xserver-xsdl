pelya/xserver-xsdl

commit 8e83eacb9e2d2c6c2b9f8cdb9e82c976a0237f24

loader: Remove unused path and name from ModuleDescPtr
Just a waste of memory. Path was never referenced at all, and name was
only used when unloading the module; we can just as well get the
module's internal idea of its name from VersionInfo.

Reviewed-by: Julien Cristau <jcristau@debian.org>
Signed-off-by: Adam Jackson <ajax@redhat.com>

hw/xfree86/loader/loadmod.c
@@ -600,17 +600,6 @@ LoadSubModule(void *_parent, const char *module,
     return submod;
 }
 
-static ModuleDescPtr
-NewModuleDesc(const char *name)
-{
-    ModuleDescPtr mdp = calloc(1, sizeof(ModuleDesc));
-
-    if (mdp)
-        mdp->name = xstrdup(name);
-
-    return mdp;
-}
-
 ModuleDescPtr
 DuplicateModule(ModuleDescPtr mod, ModuleDescPtr parent)
 {
@@ -619,7 +608,7 @@ DuplicateModule(ModuleDescPtr mod, ModuleDescPtr parent)
     if (!mod)
         return NULL;
 
-    ret = NewModuleDesc(mod->name);
+    ret = calloc(1, sizeof(ModuleDesc));
     if (ret == NULL)
         return NULL;
 
@@ -632,7 +621,6 @@ DuplicateModule(ModuleDescPtr mod, ModuleDescPtr parent)
     ret->sib = DuplicateModule(mod->sib, parent);
     ret->parent = parent;
     ret->VersionInfo = mod->VersionInfo;
-    ret->path = strdup(mod->path);
 
     return ret;
 }
@@ -726,7 +714,7 @@ LoadModule(const char *module, void *options, const XF86ModReqInfo *modreq,
             *errmaj = LDR_BADUSAGE;
         goto LoadModule_fail;
     }
-    ret = NewModuleDesc(name);
+    ret = calloc(1, sizeof(ModuleDesc));
     if (!ret) {
         if (errmaj)
             *errmaj = LDR_NOMEM;
@@ -773,7 +761,6 @@ LoadModule(const char *module, void *options, const XF86ModReqInfo *modreq,
     ret->handle = LoaderOpen(found, errmaj);
     if (ret->handle == NULL)
         goto LoadModule_fail;
-    ret->path = strdup(found);
 
     /* drop any explicit suffix from the module name */
     p = strchr(name, '.');
@@ -857,31 +844,31 @@ void
 UnloadModule(void *_mod)
 {
     ModuleDescPtr mod = _mod;
+    const char *name;
 
     if (mod == (ModuleDescPtr) 1)
         return;
 
-    if (mod == NULL || mod->name == NULL)
+    if (mod == NULL)
         return;
 
+    name = mod->VersionInfo->modname;
+
     if (mod->parent)
-        LogMessageVerbSigSafe(X_INFO, 3, "UnloadSubModule: \"%s\"\n",
-                              mod->name);
+        LogMessageVerbSigSafe(X_INFO, 3, "UnloadSubModule: \"%s\"\n", name);
     else
-        LogMessageVerbSigSafe(X_INFO, 3, "UnloadModule: \"%s\"\n", mod->name);
+        LogMessageVerbSigSafe(X_INFO, 3, "UnloadModule: \"%s\"\n", name);
 
     if (mod->TearDownData != ModuleDuplicated) {
         if ((mod->TearDownProc) && (mod->TearDownData))
             mod->TearDownProc(mod->TearDownData);
-        LoaderUnload(mod->name, mod->handle);
+        LoaderUnload(name, mod->handle);
     }
 
     if (mod->child)
         UnloadModule(mod->child);
     if (mod->sib)
         UnloadModule(mod->sib);
-    free(mod->path);
-    free(mod->name);
     free(mod);
 }
 

hw/xfree86/loader/loaderProcs.h
@@ -59,8 +59,6 @@ typedef struct module_desc {
     struct module_desc *child;
     struct module_desc *sib;
     struct module_desc *parent;
-    char *name;
-    char *path;
     void *handle;
     ModuleSetupProc SetupProc;
     ModuleTearDownProc TearDownProc;

commit 45e0eb4b156f2155687cce268b07f10540fc507b

loader: Handle mod->VersionInfo == NULL
This can happen when a module fails to load:

Program received signal SIGSEGV, Segmentation fault.
UnloadModule (_mod=0x5555559d9280) at ../../../../hw/xfree86/loader/loadmod.c:848
848	    name = mod->VersionInfo->modname;
(gdb) bt
#0  UnloadModule (_mod=0x5555559d9280) at ../../../../hw/xfree86/loader/loadmod.c:848
#1  0x00005555555ddd1b in LoadModule (module=module@entry=0x5555559c7ce0 "fbdev", options=0x0, modreq=modreq@entry=0x0, errmaj=errmaj@entry=0x7fffffffe8ec) at ../../../../hw/xfree86/loader/loadmod.c:824
#2  0x00005555555edfe9 in xf86LoadModules (list=list@entry=0x5555559dcf50, optlist=optlist@entry=0x0) at ../../../../hw/xfree86/common/xf86Init.c:1506
#3  0x00005555555ee7bc in InitOutput (pScreenInfo=pScreenInfo@entry=0x5555559abf80 <screenInfo>, argc=argc@entry=4, argv=argv@entry=0x7fffffffeb18) at ../../../../hw/xfree86/common/xf86Init.c:484
#4  0x00005555555a885c in dix_main (argc=4, argv=0x7fffffffeb18, envp=<optimized out>) at ../../dix/main.c:197
#5  0x00007ffff5d582b1 in __libc_start_main (main=0x555555593130 <main>, argc=4, argv=0x7fffffffeb18, init=<optimized out>, fini=<optimized out>, rtld_fini=<optimized out>, stack_end=0x7fffffffeb08) at ../csu/libc-start.c:291
#6  0x000055555559316a in _start ()

Fixes: 8e83eacb9e2d ("loader: Remove unused path and name from ModuleDescPtr")
Signed-off-by: Michel Dänzer <michel.daenzer@amd.com>
Reviewed-by: Peter Hutterer <peter.hutterer@who-t.net>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

hw/xfree86/loader/loadmod.c
@@ -837,7 +837,6 @@ void
 UnloadModule(void *_mod)
 {
     ModuleDescPtr mod = _mod;
-    const char *name;
 
     if (mod == (ModuleDescPtr) 1)
         return;
@@ -845,17 +844,19 @@ UnloadModule(void *_mod)
     if (mod == NULL)
         return;
 
-    name = mod->VersionInfo->modname;
+    if (mod->VersionInfo) {
+        const char *name = mod->VersionInfo->modname;
 
-    if (mod->parent)
-        LogMessageVerbSigSafe(X_INFO, 3, "UnloadSubModule: \"%s\"\n", name);
-    else
-        LogMessageVerbSigSafe(X_INFO, 3, "UnloadModule: \"%s\"\n", name);
+        if (mod->parent)
+            LogMessageVerbSigSafe(X_INFO, 3, "UnloadSubModule: \"%s\"\n", name);
+        else
+            LogMessageVerbSigSafe(X_INFO, 3, "UnloadModule: \"%s\"\n", name);
 
-    if (mod->TearDownData != ModuleDuplicated) {
-        if ((mod->TearDownProc) && (mod->TearDownData))
-            mod->TearDownProc(mod->TearDownData);
-        LoaderUnload(name, mod->handle);
+        if (mod->TearDownData != ModuleDuplicated) {
+            if ((mod->TearDownProc) && (mod->TearDownData))
+                mod->TearDownProc(mod->TearDownData);
+            LoaderUnload(name, mod->handle);
+        }
     }
 
     if (mod->child)