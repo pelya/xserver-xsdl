jigpu/xserver

xwayland: Handle tablet_tool events
Translates Wayland tablet events into corresponding X11 tablet events. As
with the prior commit, these events are modeled after those created by the
xf86-input-wacom driver to maximize compatibility with existing applications.

FIXME: Note the fixme in tablet_tool_proximity_in...

Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>

hw/xwayland/xwayland.h
@@ -191,6 +191,14 @@ struct xwl_tablet {
 struct xwl_tablet_tool {
     struct xorg_list link;
     struct zwp_tablet_tool_v2 *tool;
+    DeviceIntPtr xdevice;
+    uint32_t x;
+    uint32_t y;
+    float pressure;
+    float tilt_x;
+    float tilt_y;
+    float rotation;
+    float slider;
 };
 
 struct xwl_tablet_pad {

pelya/xserver-xsdl

Signed-off-by: Carlos Garnacho <carlosg@gnome.org>
Reviewed-by: Peter Hutterer <peter.hutterer@who-t.net>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
Acked-by: Ping Cheng <ping.cheng@wacom.com>

hw/xwayland/xwayland.h
@@ -196,6 +196,15 @@ struct xwl_tablet_tool {
     struct xorg_list link;
     struct zwp_tablet_tool_v2 *tool;
     struct xwl_seat *seat;
+
+    DeviceIntPtr xdevice;
+    uint32_t x;
+    uint32_t y;
+    uint32_t pressure;
+    float tilt_x;
+    float tilt_y;
+    float rotation;
+    float slider;
 };
 
 struct xwl_tablet_pad {