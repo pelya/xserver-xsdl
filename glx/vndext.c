089a1b0f9fa2e80457dc3d5f05c84565c9113a21
Emil Velikov <emil.velikov@collabora.com>
2018-02-19 15:18:10 +0000

    glx: set the version we implement in GlxServerExports
    Set the major/minor version it's currently implemented, not the one
    provided by the GLXSERVER_VENDOR_ABI_{MAJOR,MINOR}_VERSION macros.
    
    Those are identical for now, but can change in the future.
    
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- glx/vndext.c
+++ glx/vndext.c
@@ -277,8 +277,8 @@ GlxFreeServerImports(GlxServerImports *imports)
 }
 
 _X_EXPORT const GlxServerExports glxServer = {
-    .majorVersion = GLXSERVER_VENDOR_ABI_MAJOR_VERSION,
-    .minorVersion = GLXSERVER_VENDOR_ABI_MINOR_VERSION,
+    .majorVersion = 0,
+    .minorVersion = 0,
 
     .extensionInitCallback = &vndInitCallbackList,
 
