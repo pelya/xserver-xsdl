pelya/xserver-xsdl

commit 44d5f2eb8a2f92571698adec39ac569b71da5a1b

xf86-video-modesetting: Support new vblank kernel API [v2]
drmCrtcGetSequence returns the current vblank sequence and time.

drmCrtcQueueSequence queues an event for delivery at a specified
vblank sequence.

Use these (when available) in preference to drmWaitVBlank.

v2: Remove FIRST_PIXEL_OUT_FLAG. This has been removed from the kernel
    API.

Reviewed-by: Adam Jackson <ajax@redhat.com>
Signed-off-by: Keith Packard <keithp@keithp.com>

hw/xfree86/drivers/modesetting/vblank.c
@@ -182,6 +182,21 @@ ms_get_kernel_ust_msc(xf86CrtcPtr crtc,
     drmVBlank vbl;
     int ret;
 
+#ifdef DRM_IOCTL_CRTC_QUEUE_SEQUENCE
+    if (ms->has_queue_sequence || !ms->tried_queue_sequence) {
+        uint64_t ns;
+        ms->tried_queue_sequence = TRUE;
+
+        ret = drmCrtcGetSequence(ms->fd, drmmode_crtc->mode_crtc->crtc_id,
+                                 msc, &ns);
+        if (ret != -1 || errno != ENOTTY) {
+            ms->has_queue_sequence = TRUE;
+            if (ret == 0)
+                *ust = ns / 1000;
+            return ret == 0;
+        }
+    }
+#endif
     /* Get current count */
     vbl.request.type = DRM_VBLANK_RELATIVE | drmmode_crtc->vblank_pipe;
     vbl.request.sequence = 0;
@@ -211,6 +226,35 @@ ms_queue_vblank(xf86CrtcPtr crtc, ms_queue_flag flags,
 
     for (;;) {
         /* Queue an event at the specified sequence */
+#ifdef DRM_IOCTL_CRTC_QUEUE_SEQUENCE
+        if (ms->has_queue_sequence || !ms->tried_queue_sequence) {
+            uint32_t drm_flags = 0;
+            uint64_t kernel;
+            uint64_t kernel_queued;
+
+            ms->tried_queue_sequence = TRUE;
+
+            if (flags & MS_QUEUE_RELATIVE)
+                drm_flags |= DRM_CRTC_SEQUENCE_RELATIVE;
+            if (flags & MS_QUEUE_NEXT_ON_MISS)
+                drm_flags |= DRM_CRTC_SEQUENCE_NEXT_ON_MISS;
+
+            kernel = ms_crtc_msc_to_kernel_msc(crtc, msc);
+            ret = drmCrtcQueueSequence(ms->fd, drmmode_crtc->mode_crtc->crtc_id,
+                                       drm_flags,
+                                       kernel, &kernel_queued, seq);
+            if (ret == 0) {
+                if (msc_queued)
+                    *msc_queued = ms_kernel_msc_to_crtc_msc(crtc, kernel_queued);
+                return TRUE;
+            }
+
+            if (ret != -1 || errno != ENOTTY) {
+                ms->has_queue_sequence = TRUE;
+                goto check;
+            }
+        }
+#endif
         vbl.request.type = DRM_VBLANK_EVENT | drmmode_crtc->vblank_pipe;
         if (flags & MS_QUEUE_RELATIVE)
             vbl.request.type |= DRM_VBLANK_RELATIVE;
@@ -228,6 +272,9 @@ ms_queue_vblank(xf86CrtcPtr crtc, ms_queue_flag flags,
                 *msc_queued = ms_kernel_msc_to_crtc_msc(crtc, vbl.reply.sequence);
             return TRUE;
         }
+#ifdef DRM_IOCTL_CRTC_QUEUE_SEQUENCE
+    check:
+#endif
         if (errno != EBUSY) {
             ms_drm_abort_seq(scrn, seq);
             return FALSE;
@@ -446,9 +493,10 @@ ms_vblank_screen_init(ScreenPtr screen)
     modesettingEntPtr ms_ent = ms_ent_priv(scrn);
     xorg_list_init(&ms_drm_queue);
 
-    ms->event_context.version = 2;
+    ms->event_context.version = 4;
     ms->event_context.vblank_handler = ms_drm_handler;
     ms->event_context.page_flip_handler = ms_drm_handler;
+    ms->event_context.sequence_handler = ms_drm_sequence_handler;
 
     /* We need to re-register the DRM fd for the synchronisation
      * feedback on every server generation, so perform the

commit efe9e3e9ff96a06f50d94dc93ad2c19085fa3d09

modesetting: Fix fallback for lack of new vblank kernel API.
Turns out that the kernel DRM ioctl handling returns EINVAL
instead of ENOTTY if one tries to call the new drmCrtcGetSequence()
or drmCrtcQueueSequence() ioctl's introduced in Linux 4.15 on an
older kernel where they are missing. This causes the fallback code
not to fall back to the old drmWaitVblank() ioctl and thereby
failure of vblank stuff.

E.g., on Linux 4.13, glxgears -info runs unthrottled at 10000 fps
instead of 60 fps. Also breakage of OML_sync_control extension.

Check for errno != EINVAL before setting has_queue_sequence = TRUE.

Additionally in case of supported drmCrtcQueueSequence(), set
has_queue_sequence = TRUE on success, or we might get at
least a temporary failure in ms_queue_vblank().

One slight ambiguity is that we can also get EINVAL if
drm_crtc_vblank_get() fails in the kernel, so if that
happened at first invocation of the new api, we'd fall
back to drmWaitVblank() and then fail there, instead of
failing in the new api, but the end result would be the
same.

Fixes: 44d5f2eb8a2f ("xf86-video-modesetting: Support new vblank kernel API [v2]")
Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
Cc: Keith Packard <keithp@keithp.com>
Reviewed-by: Keith Packard <keithp@keithp.com>

hw/xfree86/drivers/modesetting/vblank.c
@@ -189,7 +189,7 @@ ms_get_kernel_ust_msc(xf86CrtcPtr crtc,
 
         ret = drmCrtcGetSequence(ms->fd, drmmode_crtc->mode_crtc->crtc_id,
                                  msc, &ns);
-        if (ret != -1 || errno != ENOTTY) {
+        if (ret != -1 || (errno != ENOTTY && errno != EINVAL)) {
             ms->has_queue_sequence = TRUE;
             if (ret == 0)
                 *ust = ns / 1000;
@@ -246,10 +246,11 @@ ms_queue_vblank(xf86CrtcPtr crtc, ms_queue_flag flags,
             if (ret == 0) {
                 if (msc_queued)
                     *msc_queued = ms_kernel_msc_to_crtc_msc(crtc, kernel_queued);
+                ms->has_queue_sequence = TRUE;
                 return TRUE;
             }
 
-            if (ret != -1 || errno != ENOTTY) {
+            if (ret != -1 || (errno != ENOTTY && errno != EINVAL)) {
                 ms->has_queue_sequence = TRUE;
                 goto check;
             }