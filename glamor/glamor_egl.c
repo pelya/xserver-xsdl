pelya/xserver-xsdl

commit d11d5bb80d21f2aaa54ddf2f26a50aa6ef31d2fa

glamor: Hide new DRI behind Option "Debug" "dmabuf_capable"
... for xfree86, at least for now. Things appear to work for Xwayland
but not yet for modesetting. Hopefully we can fix that before 1.20 but
in the meantime this makes testing both paths easier than a rebuild.

Signed-off-by: Adam Jackson <ajax@redhat.com>

glamor/glamor_egl.c
@@ -35,6 +35,7 @@
 #include <sys/ioctl.h>
 #include <errno.h>
 #include <xf86.h>
+#include <xf86Priv.h>
 #include <xf86drm.h>
 #define EGL_DISPLAY_NO_X_MESA
 
@@ -960,7 +961,8 @@ glamor_egl_init(ScrnInfoPtr scrn, int fd)
                                 "EGL_EXT_image_dma_buf_import") &&
         epoxy_has_egl_extension(glamor_egl->display,
                                 "EGL_EXT_image_dma_buf_import_modifiers"))
-        glamor_egl->dmabuf_capable = TRUE;
+        glamor_egl->dmabuf_capable = !!strstr(xf86Info.debug,
+                                              "dmabuf_capable");
 #endif
 
     glamor_egl->saved_free_screen = scrn->FreeScreen;

hw/xfree86/man/xorg.conf.man
@@ -498,6 +498,9 @@ The options recognised by this section are:
 .BI "Option \*qDebug\*q  \*q" string \*q
 This comma-separated list provides a way to control various debugging switches
 from the config file.
+At the moment the only defined value is
+.B dmabuf_capable
+which instructs glamor to enable some unstable buffer management code.
 .TP 7
 .BI "Option \*qDefaultServerLayout\*q  \*q" layout\-id \*q
 This specifies the default

commit 258081759fa70202a2e6134eeee4fe25ca54bedb

glamor: sanitze handling of "Debug" Option for "dmabuf_capable"
With the appearance of the this new Option the X Server would crash if the
option is not set.

Default dmabuf_capable to off/FALSE for now - a user without knowledge about
this option does not want to enable it until its save.

Fixes: d11d5bb80 ("glamor: Hide new DRI behind Option "Debug" "dmabuf_capable")
Signed-off-by: Tobias Klausmann <tobias.johannes.klausmann@mni.thm.de>
Reviewed-by: Adam Jackson <ajax@redhat.com>

glamor/glamor_egl.c
@@ -960,9 +960,13 @@ glamor_egl_init(ScrnInfoPtr scrn, int fd)
     if (epoxy_has_egl_extension(glamor_egl->display,
                                 "EGL_EXT_image_dma_buf_import") &&
         epoxy_has_egl_extension(glamor_egl->display,
-                                "EGL_EXT_image_dma_buf_import_modifiers"))
-        glamor_egl->dmabuf_capable = !!strstr(xf86Info.debug,
-                                              "dmabuf_capable");
+                                "EGL_EXT_image_dma_buf_import_modifiers")) {
+       if (xf86Info.debug != NULL)
+           glamor_egl->dmabuf_capable = !!strstr(xf86Info.debug,
+                                                "dmabuf_capable");
+        else
+            glamor_egl->dmabuf_capable = FALSE;
+    }
 #endif
 
     glamor_egl->saved_free_screen = scrn->FreeScreen;